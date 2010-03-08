39a78c3d9584471ea0a9837000b0bd3a51f8c35d
Bastian Eicher <bastian@eicher.net>
2010-03-08 02:02:37 +0000

    Added collection logic for Languages

--- vs/Model/TargetBase.cs
+++ vs/Model/TargetBase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
+using System.Text;
 using System.Xml.Serialization;
 
 namespace ZeroInstall.Model
@@ -12,13 +14,40 @@ namespace ZeroInstall.Model
     public abstract class TargetBase
     {
         #region Properties
+        private readonly List<CultureInfo> _languages = new List<CultureInfo>();
         /// <summary>
         /// The natural language(s) which an <see cref="Implementation"/> supports, as a space-separated list of languages codes (in the same format as used by the $LANG environment variable).
         /// </summary>
         /// <example>For example, the value "en_GB fr" would be used for a package supporting British English and French.</example>
         [Category("Release"), Description("The natural language(s) which an implementation supports, as a space-separated list of languages codes (in the same format as used by the $LANG environment variable).")]
-        [XmlAttribute("langs")]
-        public string Languages { get; set; }
+        [XmlIgnore]
+        public IList<CultureInfo> Languages { get { return _languages; } }
+
+        /// <summary>Used for XML serialization.</summary>
+        /// <seealso cref="Architecture"/>
+        [XmlAttribute("langs"), Browsable(false)]
+        public string LanguagesString
+        {
+            get
+            {
+                // Serialize list as string split by spaces
+                var output = new StringBuilder();
+                foreach (CultureInfo info in _languages)
+                {
+                    output.Append(info + " ");
+                }
+
+                // Return without trailing space
+                return output.ToString(0, output.Length - 1);
+            }
+            set
+            {
+                // Replace language list by parsing input string split by spaces
+                _languages.Clear();
+                foreach (string lang in value.Split(' '))
+                    _languages.Add(new CultureInfo(lang));
+            }
+        }
 
         /// <summary>
         /// For platform-specific binaries, the platform for which an <see cref="Implementation"/> was compiled, in the form os-cpu. Either the os or cpu part may be *, which will make it available on any OS or CPU. 
