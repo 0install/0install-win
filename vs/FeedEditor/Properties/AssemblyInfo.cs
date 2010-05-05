b5e18c52cea2e878531cef2d57875bb6d13a2acb
Bastian Eicher <bastian@eicher.net>
2010-05-05 00:07:19 +0000

    - Forgot to commit a modified project file - Updated some project descriptions

--- vs/FeedEditor/Properties/AssemblyInfo.cs
+++ vs/FeedEditor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Zero Install Feed Editor")]
-[assembly: AssemblyDescription("An Editor for Zero Install feed files.")]
+[assembly: AssemblyDescription("An editor for Zero Install feed files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("0install.net")]
 [assembly: AssemblyProduct("Zero Install")]
--- vs/Injector/Properties/AssemblyInfo.cs
+++ vs/Injector/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Zero Install Injector Library")]
-[assembly: AssemblyDescription("Methods for launching Zero Install implementations.")]
+[assembly: AssemblyDescription("Methods for downloading and launching Zero Install implementations.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("0install.net")]
 [assembly: AssemblyProduct("Zero Install")]
