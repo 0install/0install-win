c3136535e5a54ac41d61014f9ec3eee6cee6e134
Bastian Eicher <bastian@eicher.net>
2010-07-19 22:40:59 +0000

    Tweaked documentation generation

--- build.cmd
+++ build.cmd
@@ -1,15 +1,13 @@
 @echo off
-::Compiles the source code and then creates an installer or source code documentation.
-::Use command-line argument "doc" to create source code documentation.
+::Compiles the source code and then creates an installer.
+::Use command-line argument "+doc" to additionally create source code documentation.
 
-if not "%1"=="doc" (
-  echo.
-  call "%~dp0vs\build.cmd" Release
-  echo.
-  call "%~dp0setup\build.cmd"
-)
+echo.
+call "%~dp0vs\build.cmd" Release
+echo.
+call "%~dp0setup\build.cmd"
 
-if "%1"=="doc" (
+if "%1"=="+doc" (
   echo.
   call "%~dp0vs\build.cmd" Debug
   echo.
