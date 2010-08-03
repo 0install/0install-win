232e1cb34ca54f947d2b3d3efe73b8271699a2a4
Roland Leopold Walkling <leopold_walkling@web.de>
2010-08-03 13:51:57 +0000

    Forgot to include new files into commit.

---
+++ vs/Common/Storage/TemporaryDirectoryReplacement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Storage
+{
+    /// <summary>
+    /// Disposable class that allows operating on a newly created folder at a
+    /// specified path, and if necessary temporarily moving the original folder
+    /// to a new position.
+    /// </summary>
+    public class TemporaryDirectoryReplacement : IDisposable
+    {
+        private readonly TemporaryDirectory _tempDir;
+        private readonly TemporaryDirectoryMove _backupMove;
+
+        public string Path
+        {
+            get { return _tempDir.Path; }
+        }
+
+        public string BackupPath
+        {
+            get { return _backupMove.BackupPath; }
+        }
+
+        /// <summary>
+        /// Applies <see cref="TemporaryDirectoryMove"/> and <see cref="TemporaryDirectory"/>.
+        /// </summary>
+        /// <param name="path"></param>
+        public TemporaryDirectoryReplacement(string path)
+        {
+            _backupMove = new TemporaryDirectoryMove(path);
+            _tempDir = new TemporaryDirectory(path);
+        }
+
+        /// <summary>
+        /// Applies the Dispose methods of <see cref="TemporaryDirectoryMove"/> and <see cref="TemporaryDirectory"/>
+        /// </summary>
+        public void Dispose()
+        {
+            _tempDir.Dispose();
+            _backupMove.Dispose();
+        }
+    }
+}
---
+++ vs/Test.Frontend/DownloadBroker/PackageBuilderManifestExtension.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Helpers;
+using ZeroInstall.Store.Implementation;
+using ZeroInstall.Model;
+using System.IO;
+
+namespace ZeroInstall.DownloadBroker
+{
+    class HierarchyToManifest : HierarchyVisitor
+    {
+        readonly StreamWriter writer;
+
+        public HierarchyToManifest(Stream target)
+        {
+            writer = new StreamWriter(target) { NewLine = "\n" };
+        }
+
+        public override void VisitRoot(RootEntry entry)
+        {
+            visitChildren(entry);
+            writer.Flush();
+        }
+        public override void VisitFolder(FolderEntry entry)
+        {
+            ManifestNode node = new ManifestDirectory(FileHelper.UnixTime(entry.LastWriteTime), "/" + entry.RelativePath.Replace("\\", "/"));
+            writer.WriteLine(ManifestFormat.Sha256.GenerateEntryForNode(node));
+            visitChildren(entry);
+        }
+        public override void VisitFile(FileEntry entry)
+        {
+            ManifestNode node = null;
+            string hash;
+            long size;
+            using (var entryData = new MemoryStream(entry.Content))
+            {
+                size = entryData.Length;
+                hash = FileHelper.ComputeHash(entryData, ManifestFormat.Sha256.HashAlgorithm);
+            }
+            if (entry.IsExecutable()) node = new ManifestExecutableFile(hash, FileHelper.UnixTime(entry.LastWriteTime), size, entry.Name);
+            else node = new ManifestFile(hash, FileHelper.UnixTime(entry.LastWriteTime), size, entry.Name);
+            writer.WriteLine(ManifestFormat.Sha256.GenerateEntryForNode(node));
+        }
+    }
+
+    public static class PackageBuilderManifestExtension
+    {
+        public static ManifestDigest ComputePackageDigest(PackageBuilder package)
+        {
+            using (var dotFile = new MemoryStream())
+            {
+                WriteHierarchyManifestToStream(package, dotFile);
+                dotFile.Seek(0, SeekOrigin.Begin);
+                return new ManifestDigest(ManifestFormat.Sha256.Prefix + FileHelper.ComputeHash(dotFile, ManifestFormat.Sha256.HashAlgorithm));
+            }
+        }
+
+        private static void WriteHierarchyManifestToStream(PackageBuilder package, Stream dotFile)
+        {
+            var manifestCollector = new HierarchyToManifest(dotFile);
+            package.Hierarchy.AcceptVisitor(manifestCollector);
+        }
+    }
+}
