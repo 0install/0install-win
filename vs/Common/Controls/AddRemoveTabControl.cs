eefd64ab671f92e5b2a0523e0d61df6070f1cefa
Bastian Eicher <bastian@eicher.net>
2010-09-13 01:21:43 +0000

    Added missing copyright comments

--- vs/Common/Controls/AddRemoveTabControl.cs
+++ vs/Common/Controls/AddRemoveTabControl.cs
@@ -1,18 +1,23 @@
 ﻿/*
  * Copyright 2010 Simon E. Silva Lauinger
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 using System;
@@ -21,11 +26,8 @@ using System.Windows.Forms;
 namespace Common.Controls
 {
     #region Delegates
-
     public delegate void NewTabEventHandler(TabControl sender, TabPage createdTabPage);
-
     #endregion
-
 
     /// <summary>
     /// A tab control that can add and remove tabs.
@@ -37,23 +39,19 @@ namespace Common.Controls
     public partial class AddRemoveTabControl : UserControl
     {
         #region Properties
-
         /// <summary>
         /// The last inserted <see cref="TabPage"/>. <see langword="null"/> if no <see cref="TabPage"/> was inserted.
         /// </summary>
         public TabPage LastInsertedTabPage { get; private set; }
-
         #endregion
 
         #region Events
-
         public event NewTabEventHandler NewTabCreated;
 
         private void OnNewTabCreated()
         {
             if (NewTabCreated != null) NewTabCreated(tabControl1, LastInsertedTabPage);
         }
-
         #endregion
 
         public AddRemoveTabControl()
