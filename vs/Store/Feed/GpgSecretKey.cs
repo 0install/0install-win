7f77dbd513dd2d44e62126a3a502798f0b7d3cb7
Simon E. Silva Lauinger <simon@silvalauinger.de>
2010-09-20 00:48:35 +0000

    GnuPG: output of ListSecretKeys will now be parsed and returns a list of GpgSecretKey objects. Added new struct: GpgSecretKey

---
+++ vs/Store/Feed/GpgSecretKey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ZeroInstall.Store.Feed
+{
+    public enum KeyType
+    {
+        Dsa,
+        Elgamal,
+        Rsa
+    } ;
+
+    public struct GpgSecretKey
+    {
+        public int BitLength;
+        public KeyType KeyType;
+        public string MainSigningKey;
+        public DateTime CreationDate;
+        public string Owner;
+        public string EmailAdress;
+
+        public override string ToString()
+        {
+            return Owner;
+        }
+    }
+}
