3b87b93c2649d26a397eacdbed405c2d6a4adf0d
Bastian Eicher <bastian@eicher.net>
2010-10-24 18:45:00 +0000

    Incremented version number to 0.50.1

--- vs/DownloadBroker/Properties/AssemblyInfo.cs
+++ vs/DownloadBroker/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("baa28528-7b64-49f6-8d79-b07c465dc95c")]
 
 // Version information
-[assembly: AssemblyVersion("0.50.0")]
-[assembly: AssemblyFileVersion("0.50.0")]
+[assembly: AssemblyVersion("0.50.1")]
+[assembly: AssemblyFileVersion("0.50.1")]
