94fa23166b09a0eb382677a627d3687961cd3f40
Bastian Eicher <bastian@eicher.net>
2010-11-02 20:03:19 +0000

    Redesigned HintTextBox to use own Text for HintText instead of a label to work around WinForms validation focus bug

--- vs/Common/Controls/HintTextBox.cs
+++ vs/Common/Controls/HintTextBox.cs
@@ -37,40 +37,34 @@ namespace Common.Controls
         #region Events
         protected override void OnEnter(EventArgs e)
         {
-            // No hint when the cursor is in the text box
-            _hintLabel.Visible = false;
+            // Remove the hint when entering the TextBox
+            HideHintText();
 
-            base.OnEnter(e);
+            base.OnGotFocus(e);
         }
 
         protected override void OnLeave(EventArgs e)
         {
-            // Restore hint when the cursor leaves the text box and the field is empty
-            _hintLabel.Visible = string.IsNullOrEmpty(base.Text);
+            // Restore hint when leaving the TextBox with an empty Text
+            if (string.IsNullOrEmpty(base.Text)) ShowHintText();
 
             base.OnLeave(e);
         }
 
         protected override void OnTextChanged(EventArgs e)
         {
-            // Show hint only when the cursor is not in the text box and the field is empty
-            _hintLabel.Visible = HintTextVisible;
+            // Show clear button only if it is enabled and there is text that can be cleared
+            _buttonClear.Visible = _showClearButton && !string.IsNullOrEmpty(base.Text) && !IsHintTextVisible;
 
-            // Show clear button only if it is enabled and the field is not empty
-            _buttonClear.Visible = _showClearButton && !string.IsNullOrEmpty(base.Text);
+            // Prevent displaying of hint text from raising events
+            if (_suppressTextChangedEvent) return;
 
             base.OnTextChanged(e);
         }
 
-        protected override void OnFontChanged(EventArgs e)
-        {
-            _hintLabel.Font = Font;
-
-            base.OnFontChanged(e);
-        }
-
         protected override void OnBackColorChanged(EventArgs e)
         {
+            // Make sure the clear button has the same background as the TextBox
             _buttonClear.BackColor = BackColor;
 
             base.OnBackColorChanged(e);
@@ -78,11 +72,8 @@ namespace Common.Controls
         #endregion
 
         #region Variables
-        private readonly Label _hintLabel = new Label
-        {
-            Location = new Point(-2, 1), Size = new Size(85, 18), Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom,
-            ForeColor = SystemColors.GrayText
-        };
+        /// <summary>Prevents <see cref="OnTextChanged"/> from raising any events.</summary>
+        private bool _suppressTextChangedEvent;
 
         private readonly PictureBox _buttonClear = new PictureBox
         {
@@ -93,29 +84,49 @@ namespace Common.Controls
         #endregion
 
         #region Properties
+        /// <inheritdoc/>
+        public override string Text
+        {
+            get { return IsHintTextVisible ? "" : base.Text; }
+            set
+            {
+                // Update the visibility of the hint text when the text is changed externally
+                if (string.IsNullOrEmpty(value) && !Focused) ShowHintText();
+                else HideHintText();
+
+                // Inform the underlying TextBox
+                base.Text = value;
+            }
+        }
+
+        private string _hintText;
         /// <summary>
         /// A text to be displayed in <see cref="SystemColors.GrayText"/> when <see cref="TextBox.Text"/> is empty.
         /// </summary>
         [Category("Appearance"), Description("A text to be displayed in gray when Text is empty."), Localizable(true)]
         public string HintText
         {
-            get { return _hintLabel.Text; } set { _hintLabel.Text = value; }
+            get { return _hintText; }
+            set
+            {
+                _hintText = value;
+
+                // Update the hint text on-screen if it is already visible
+                if (IsHintTextVisible) ShowHintText();
+            }
         }
 
         /// <summary>
         /// Indicates whether the <see cref="HintText"/> is currently visible.
         /// </summary>
         [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public bool HintTextVisible
-        {
-            get { return !Focused && string.IsNullOrEmpty(base.Text); }
-        }
+        public bool IsHintTextVisible { get; private set; }
 
         private bool _showClearButton;
         /// <summary>
-        /// Controls whether the clear button is shown when <see cref="TextBox.Text"/> is not empty.
+        /// Controls whether the clear button is shown. Remains invisible when the HintText is visible.
         /// </summary>
-        [DefaultValue(false), Category("Appearance"), Description("Controls whether the clear button is shown when Text is not empty.")]
+        [DefaultValue(false), Category("Appearance"), Description("Controls whether the clear button is shown. Remains invisible when the HintText is visible.")]
         public bool ShowClearButton
         {
             get { return _showClearButton; }
@@ -123,8 +134,8 @@ namespace Common.Controls
             {
                 _showClearButton = value;
 
-                // Show clear button only if it is enabled and the field is not empty
-                _buttonClear.Visible = value && !string.IsNullOrEmpty(base.Text);
+                // Show clear button only if it is enabled and there is text that can be cleared
+                _buttonClear.Visible = value && !string.IsNullOrEmpty(base.Text) && !IsHintTextVisible;
             }
         }
         #endregion
@@ -132,7 +143,9 @@ namespace Common.Controls
         #region Constructor
         public HintTextBox()
         {
-            _hintLabel.Click += delegate { Focus(); };
+            // Since TextBoxes start off empty, hint texts are visible by default
+            IsHintTextVisible = true;
+
             _buttonClear.Click += delegate
             {
                 Focus();
@@ -140,10 +153,33 @@ namespace Common.Controls
                 // Only clear the text if focus change was possible (might be prevented by validation)
                 if (Focused) Clear();
             };
-
-            Controls.Add(_hintLabel);
             Controls.Add(_buttonClear);
         }
         #endregion
+
+        private void ShowHintText()
+        {
+            IsHintTextVisible = true;
+
+            // Show the hint text without raising events
+            _suppressTextChangedEvent = true;
+            base.Text = _hintText;
+            _suppressTextChangedEvent = false;
+            ForeColor = SystemColors.GrayText;
+        }
+
+        private void HideHintText()
+        {
+            // Don't try to clear the TextBox if isn't displaying the hint text - it might contain real data
+            if (!IsHintTextVisible) return;
+
+            IsHintTextVisible = false;
+
+            // Remove the hint text without raising events
+            _suppressTextChangedEvent = true;
+            base.Text = "";
+            _suppressTextChangedEvent = false;
+            ForeColor = SystemColors.ControlText;
+        }
     }
 }
