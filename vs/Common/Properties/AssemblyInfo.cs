a25a3ab0c5ebb866f0aa57d13eca7ddf893bbb37
Bastian Eicher <bastian@eicher.net>
2010-11-04 00:40:15 +0000

    Cleaned up AssemblyInfo files

--- vs/Common/Properties/AssemblyInfo.cs
+++ vs/Common/Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
-using System.Security.Permissions;
 
 // Assembly info
 [assembly: AssemblyTitle("Common functions")]
@@ -18,7 +17,5 @@ using System.Security.Permissions;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Security settings
-[assembly: FileIOPermission(SecurityAction.RequestMinimum, Unrestricted = true)]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode = true)]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
