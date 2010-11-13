aff638fe798242952720d6e1840e8a1dd8d695b3
Bastian Eicher <bastian@eicher.net>
2010-11-13 18:37:54 +0000

    - Added new SetLocalizableString command class - Extended Test.Common unit tests

---
+++ vs/Test.Common/Undo/SetLocalizableStringTest.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Copyright 2006-2010 Bastian Eicher
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System.Globalization;
+using Common.Collections;
+using NUnit.Framework;
+
+namespace Common.Undo
+{
+    /// <summary>
+    /// Contains test methods for <see cref="SetLocalizableString"/>.
+    /// </summary>
+    [TestFixture]
+    public class SetLocalizableStringTest
+    {
+        /// <summary>
+        /// Makes sure <see cref="SetLocalizableString"/> correctly performs executions and undos.
+        /// </summary>
+        [Test]
+        public void TestExecute()
+        {
+            var collection = new LocalizableStringCollection
+            {
+                "neutralValue1",
+                {"americaValue1", new CultureInfo("en-US")}
+            };
+
+            var neutralCommand = new SetLocalizableString(collection, new LocalizableString("neutralValue2"));
+            var americanCommand = new SetLocalizableString(collection, new LocalizableString("americaValue2", new CultureInfo("en-US")));
+
+            neutralCommand.Execute();
+            Assert.AreEqual("neutralValue2", collection.GetExactLanguage(CultureInfo.InvariantCulture));
+
+            neutralCommand.Undo();
+            Assert.AreEqual("neutralValue1", collection.GetExactLanguage(CultureInfo.InvariantCulture));
+
+            americanCommand.Execute();
+            Assert.AreEqual("americaValue2", collection.GetExactLanguage(new CultureInfo("en-US")));
+
+            americanCommand.Undo();
+            Assert.AreEqual("americaValue1", collection.GetExactLanguage(new CultureInfo("en-US")));
+        }
+    }
+}
