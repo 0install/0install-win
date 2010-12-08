ced0ef50d8458d33bae6c171eae2125f2d62fe7c
Bastian Eicher <bastian@eicher.net>
2010-12-08 00:37:05 +0000

    Tweaked shell scripts

--- cleanup.sh
+++ cleanup.sh
@@ -2,10 +2,10 @@
 #Removes compilation artifacts and other temporary files.
 cd `dirname $0`
 
-#Clear binaries (leave Documentation and Portable intact)
+# Clear binaries (leave Documentation and Portable intact)
 rm -rf build/Backend build/Frontend build/Tools build/Setup
 
-#Clear object caches
+# Clear object caches
 rm -f vs/*.cache
 rm -rf vs/*/obj
 rm -f vs/*/.pidb
--- test.sh
+++ test.sh
@@ -2,4 +2,7 @@
 #Runs the unit tests.
 cd `dirname $0`
 
+# Store all temporary files created by unit tests in a separate directory
+export TMPDIR=`mktemp -d -t 0install-unit-tests.XXXXXXXXXX` || exit 1
+
 nunit-console UnitTests.nunit
