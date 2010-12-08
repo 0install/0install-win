bd6bde67f2e597227228987fe9b91fa6422adf3d
Bastian Eicher <bastian@eicher.net>
2010-12-08 01:11:53 +0000

    Removed custom temp directory from test.sh script

--- test.sh
+++ test.sh
@@ -2,12 +2,4 @@
 #Runs the unit tests.
 cd `dirname $0`
 
-# Store all temporary files created by unit tests in a separate (empty) directory
-export TMPDIR=/tmp/0install-unit-tests
-if [ -d "$TMPDIR" ]; then
-  rm -rf $TMPDIR || exit 1
-fi
-mkdir $TMPDIR || exit 1
-
-# Clean up temporary files if all unit tests passed
-nunit-console UnitTests.nunit && rm -rf $TMPDIR
+nunit-console UnitTests.nunit
