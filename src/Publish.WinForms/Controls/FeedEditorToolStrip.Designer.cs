14c4588de6d7373ce2fe2e72c9d1593cffc6decc
Simon E. Silva Lauinger <simon@silvalauinger.de>
2011-05-02 23:27:26 +0000

    Added images for an undo and an redo button. Added specialized ToolStrip (FeedEditorToolStrip) for the feed editor.

---
+++ src/Publish.WinForms/Controls/FeedEditorToolStrip.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ZeroInstall.Publish.WinForms.Controls
+{
+    partial class FeedEditorToolStrip
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FeedEditorToolStrip));
+            this.buttonNew = new System.Windows.Forms.ToolStripButton();
+            this.buttonOpen = new System.Windows.Forms.ToolStripButton();
+            this.buttonSave = new System.Windows.Forms.ToolStripButton();
+            this.buttonSaveAs = new System.Windows.Forms.ToolStripButton();
+            this.buttonUndo = new System.Windows.Forms.ToolStripButton();
+            this.buttonRedo = new System.Windows.Forms.ToolStripButton();
+            this.labelGnuPG = new System.Windows.Forms.ToolStripLabel();
+            this.comboBoxGnuPG = new System.Windows.Forms.ToolStripComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonNew.Image = ((System.Drawing.Image)(resources.GetObject("buttonNew.Image")));
+            this.buttonNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(23, 22);
+            this.buttonNew.Text = "New";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonOpen.Image = global::ZeroInstall.Publish.WinForms.Properties.Resources.OpenButton;
+            this.buttonOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(23, 22);
+            this.buttonOpen.Text = "Open";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonSave.Image = global::ZeroInstall.Publish.WinForms.Properties.Resources.SaveButton;
+            this.buttonSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(23, 22);
+            this.buttonSave.Text = "Save";
+            // 
+            // buttonSaveAs
+            // 
+            this.buttonSaveAs.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonSaveAs.Image = ((System.Drawing.Image)(resources.GetObject("buttonSaveAs.Image")));
+            this.buttonSaveAs.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonSaveAs.Name = "buttonSaveAs";
+            this.buttonSaveAs.Size = new System.Drawing.Size(23, 20);
+            this.buttonSaveAs.Text = "SaveAs";
+            // 
+            // buttonUndo
+            // 
+            this.buttonUndo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonUndo.Image = global::ZeroInstall.Publish.WinForms.Properties.Resources.UndoButton;
+            this.buttonUndo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonUndo.Name = "buttonUndo";
+            this.buttonUndo.Size = new System.Drawing.Size(23, 20);
+            this.buttonUndo.Text = "Undo";
+            // 
+            // buttonRedo
+            // 
+            this.buttonRedo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buttonRedo.Image = global::ZeroInstall.Publish.WinForms.Properties.Resources.RedoButton;
+            this.buttonRedo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonRedo.Name = "buttonRedo";
+            this.buttonRedo.Size = new System.Drawing.Size(23, 20);
+            this.buttonRedo.Text = "Redo";
+            // 
+            // labelGnuPG
+            // 
+            this.labelGnuPG.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.labelGnuPG.Name = "labelGnuPG";
+            this.labelGnuPG.Size = new System.Drawing.Size(68, 15);
+            this.labelGnuPG.Text = "GnuPG key:";
+            // 
+            // comboBoxGnuPG
+            // 
+            this.comboBoxGnuPG.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.comboBoxGnuPG.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxGnuPG.Name = "comboBoxGnuPG";
+            this.comboBoxGnuPG.Size = new System.Drawing.Size(121, 23);
+            // 
+            // FeedEditorToolStrip
+            // 
+            this.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buttonNew,
+            this.buttonOpen,
+            this.buttonSave,
+            this.buttonSaveAs,
+            this.buttonUndo,
+            this.buttonRedo,
+            this.labelGnuPG,
+            this.comboBoxGnuPG});
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripButton buttonNew;
+        private System.Windows.Forms.ToolStripButton buttonOpen;
+        private System.Windows.Forms.ToolStripButton buttonSave;
+        private System.Windows.Forms.ToolStripButton buttonSaveAs;
+        private System.Windows.Forms.ToolStripButton buttonUndo;
+        private System.Windows.Forms.ToolStripButton buttonRedo;
+        private System.Windows.Forms.ToolStripLabel labelGnuPG;
+        private System.Windows.Forms.ToolStripComboBox comboBoxGnuPG;
+    }
+}
---
+++ src/Publish.WinForms/Controls/FeedEditorToolStrip.cs
@@ -0,0 +1,177 @@
+﻿/*
+ * Copyright 2011 Simon E. Silva Lauinger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Common;
+using ZeroInstall.Model;
+using ZeroInstall.Store.Feeds;
+
+namespace ZeroInstall.Publish.WinForms.Controls
+{
+    /// <summary>
+    /// A specialized <see cref="ToolStrip"/> for the Feed Editor. It has <see cref="Button"/>s to create, open, save and save as a feed.
+    /// As well it has <see cref="Button"/>s to undo or redo an action. With an <see cref="ComboBox"/> the user can select an <see cref="OpenPgpSecretKey"/>.
+    /// </summary>
+    public partial class FeedEditorToolStrip : ToolStrip
+    {
+        #region Properties
+
+        /// <summary>
+        /// The selected <see cref="OpenPgpSecretKey"/> by the user.
+        /// </summary>
+        public OpenPgpSecretKey SelectedSecretKey
+        {
+            get { return _selectedSecretKey; }
+            set
+            {
+                if (!comboBoxGnuPG.Items.Contains(value)) comboBoxGnuPG.Items.Add(value);
+                comboBoxGnuPG.SelectedItem = value;
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="OpenPgpSecretKey"/>s the user can select from.
+        /// </summary>
+        public OpenPgpSecretKey[] SecretKeyValues
+        {
+            set
+            {
+                comboBoxGnuPG.Items.Clear();
+
+                comboBoxGnuPG.Items.Add(OpenPgpSecretKey.EmptyKey);
+                foreach (var openPgpSecretKey in value)
+                {
+                    comboBoxGnuPG.Items.Add(openPgpSecretKey);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Raised when a new <see cref="Feed"/> shall be created.
+        /// </summary>
+        public event SimpleEventHandler New;
+
+        /// <summary>
+        /// Raised when an existing <see cref="Feed"/> shall be opened.
+        /// </summary>
+        public event SimpleEventHandler Open;
+
+        /// <summary>
+        /// Raised when an the current editted <see cref="Feed"/> shall be saved.
+        /// </summary>
+        public event SimpleEventHandler Save;
+
+        /// <summary>
+        /// Raised when an the current editted <see cref="Feed"/> shall be saved on a new place.
+        /// </summary>
+        public event SimpleEventHandler SaveAs;
+
+        /// <summary>
+        /// Raised when the last action on the <see cref="Feed"/> shall be undone.
+        /// </summary>
+        public event SimpleEventHandler Undo;
+
+        /// <summary>
+        /// Raised when the last undo on the <see cref="Feed"/> shall be redone.
+        /// </summary>
+        public event SimpleEventHandler Redo;
+
+        /// <summary>
+        /// Raised when the user selected an other <see cref="OpenPgpSecretKey"/>.
+        /// </summary>
+        public event Action<OpenPgpSecretKey> SecretKeyChanged;
+
+        #endregion
+
+        #region Attributes
+
+        /// <summary>
+        /// The selected <see cref="OpenPgpSecretKey"/> by the user.
+        /// </summary>
+        private OpenPgpSecretKey _selectedSecretKey = OpenPgpSecretKey.EmptyKey;
+
+        #endregion
+
+        #region Constructors
+
+        public FeedEditorToolStrip()
+        {
+            InitializeComponent();
+            SetDefaultValues();
+            ConnectEvents();
+        }
+
+        public FeedEditorToolStrip(IContainer container) : this()
+        {
+            container.Add(this);
+        }
+
+        #endregion
+
+        #region Initialization
+
+        /// <summary>
+        /// Sets the default values.
+        /// </summary>
+        private void SetDefaultValues()
+        {
+            comboBoxGnuPG.Items.Add(_selectedSecretKey);
+            comboBoxGnuPG.SelectedItem = _selectedSecretKey;
+        }
+
+        /// <summary>
+        /// Connects the events to use with their methods.
+        /// </summary>
+        private void ConnectEvents()
+        {
+            buttonNew.Click += (sender, eventArgs) => { if (New != null) New(); };
+            buttonOpen.Click += (sender, eventArgs) => { if (Open != null) Open(); };
+            buttonSave.Click += (sender, eventArgs) => { if (Save != null) Save(); };
+            buttonSaveAs.Click += (sender, eventArgs) => { if (SaveAs != null) SaveAs(); };
+            buttonUndo.Click += (sender, eventArgs) => { if (Undo != null) Undo(); };
+            buttonRedo.Click += (sender, eventArgs) => { if (Redo != null) Redo(); };
+            comboBoxGnuPG.SelectedIndexChanged += SelectedSecretKeyChanged;
+        }
+
+        #endregion
+
+        #region Control events
+
+        /// <summary>
+        /// Sets the property <see cref="SelectedSecretKey"/> and raises the event <see cref="SecretKeyChanged"/>.
+        /// </summary>
+        /// <param name="sender">not used.</param>
+        /// <param name="e">not used.</param>
+        private void SelectedSecretKeyChanged(object sender, EventArgs e)
+        {
+            if (SecretKeyChanged == null) return;
+
+            var selectedKey = comboBoxGnuPG.SelectedItem ?? OpenPgpSecretKey.EmptyKey;
+
+            _selectedSecretKey = (OpenPgpSecretKey) selectedKey;
+            SecretKeyChanged((OpenPgpSecretKey) selectedKey);
+        }
+
+        #endregion
+    }
+}
