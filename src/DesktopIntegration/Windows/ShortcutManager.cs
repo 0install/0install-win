68b4ae40b03c895b0e7f68c35add373a2f435a79
Bastian Eicher <bastian@eicher.net>
2011-06-08 03:25:32 +0000

    Temporarily commented out ShortcutManager to prevent build failures on Linux (until a real solution is found)

--- src/DesktopIntegration/Windows/ShortcutManager.cs
+++ src/DesktopIntegration/Windows/ShortcutManager.cs
@@ -15,38 +15,38 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-using System;
-using IWshRuntimeLibrary;
+//using System;
+//using IWshRuntimeLibrary;
 
-namespace ZeroInstall.DesktopIntegration.Windows
-{
-    /// <summary>
-    /// Utility class for creating and modifying Windows shortcut files (.lnk).
-    /// </summary>
-    public static class ShortcutManager
-    {
-        private static readonly WshShellClass _wshShell = new WshShellClass();
+//namespace ZeroInstall.DesktopIntegration.Windows
+//{
+//    /// <summary>
+//    /// Utility class for creating and modifying Windows shortcut files (.lnk).
+//    /// </summary>
+//    public static class ShortcutManager
+//    {
+//        private static readonly WshShellClass _wshShell = new WshShellClass();
 
-        /// <summary>
-        /// Creates a new Windows shortcut.
-        /// </summary>
-        /// <param name="path">The location to place the shorcut at.</param>
-        /// <param name="target">The target the shortcut shall point to.</param>
-        /// <param name="icon">The path of the icon for the shortcut; may be <see langword="null"/>.</param>
-        /// <param name="description">The shortcut description text; may be <see langword="null"/>.</param>
-        public static void CreateShortcut(string path, string target, string icon, string description)
-        {
-            #region Sanity checks
-            if (string.IsNullOrEmpty(path)) throw new ArgumentNullException("path");
-            if (string.IsNullOrEmpty(target)) throw new ArgumentNullException("target");
-            #endregion
+//        /// <summary>
+//        /// Creates a new Windows shortcut.
+//        /// </summary>
+//        /// <param name="path">The location to place the shorcut at.</param>
+//        /// <param name="target">The target the shortcut shall point to.</param>
+//        /// <param name="icon">The path of the icon for the shortcut; may be <see langword="null"/>.</param>
+//        /// <param name="description">The shortcut description text; may be <see langword="null"/>.</param>
+//        public static void CreateShortcut(string path, string target, string icon, string description)
+//        {
+//            #region Sanity checks
+//            if (string.IsNullOrEmpty(path)) throw new ArgumentNullException("path");
+//            if (string.IsNullOrEmpty(target)) throw new ArgumentNullException("target");
+//            #endregion
 
-            var shortcut = (IWshShortcut)_wshShell.CreateShortcut(target);
-            shortcut.TargetPath = target;
-            if (description != null) shortcut.Description = description;
-            if (!string.IsNullOrEmpty(icon)) shortcut.IconLocation = icon;
-            shortcut.Save();
-        }
-    }
-}
+//            var shortcut = (IWshShortcut)_wshShell.CreateShortcut(target);
+//            shortcut.TargetPath = target;
+//            if (description != null) shortcut.Description = description;
+//            if (!string.IsNullOrEmpty(icon)) shortcut.IconLocation = icon;
+//            shortcut.Save();
+//        }
+//    }
+//}
 
