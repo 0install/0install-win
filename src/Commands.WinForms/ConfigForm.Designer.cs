bc8a8df1b3cca560bc956fe7d8e0cced8313552b
Bastian Eicher <bastian@eicher.net>
2011-06-16 22:29:33 +0000

    Added localization for ConfigForm

--- src/Commands.WinForms/ConfigForm.Designer.cs
+++ src/Commands.WinForms/ConfigForm.Designer.cs
@@ -29,49 +29,49 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigForm));
             this.propertyGrid = new System.Windows.Forms.PropertyGrid();
             this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.resetValueMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenu.SuspendLayout();
             this.SuspendLayout();
             // 
+            // buttonOK
+            // 
+            resources.ApplyResources(this.buttonOK, "buttonOK");
+            // 
+            // buttonCancel
+            // 
+            resources.ApplyResources(this.buttonCancel, "buttonCancel");
+            // 
             // propertyGrid
             // 
-            this.propertyGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.propertyGrid, "propertyGrid");
             this.propertyGrid.ContextMenuStrip = this.contextMenu;
-            this.propertyGrid.Location = new System.Drawing.Point(12, 12);
             this.propertyGrid.Name = "propertyGrid";
             this.propertyGrid.PropertySort = System.Windows.Forms.PropertySort.NoSort;
-            this.propertyGrid.Size = new System.Drawing.Size(260, 209);
-            this.propertyGrid.TabIndex = 0;
             this.propertyGrid.ToolbarVisible = false;
             // 
             // contextMenu
             // 
+            resources.ApplyResources(this.contextMenu, "contextMenu");
             this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.resetValueMenuItem});
             this.contextMenu.Name = "contextMenu";
-            this.contextMenu.Size = new System.Drawing.Size(153, 48);
             // 
             // resetValueMenuItem
             // 
+            resources.ApplyResources(this.resetValueMenuItem, "resetValueMenuItem");
             this.resetValueMenuItem.Name = "resetValueMenuItem";
-            this.resetValueMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.resetValueMenuItem.Text = "&Reset value";
             this.resetValueMenuItem.Click += new System.EventHandler(this.resetValueMenuItem_Click);
             // 
             // ConfigForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.propertyGrid);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
-            this.MinimumSize = new System.Drawing.Size(240, 180);
             this.Name = "ConfigForm";
-            this.Text = "Zero Install configuration";
             this.Controls.SetChildIndex(this.propertyGrid, 0);
             this.Controls.SetChildIndex(this.buttonOK, 0);
             this.Controls.SetChildIndex(this.buttonCancel, 0);
