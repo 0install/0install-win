62ece0d0df00d6ccc29bb4bdbadfa89cdf683f31
Bastian Eicher <bastian@eicher.net>
2011-06-19 04:43:22 +0000

    Replaced InvalidOperationException with InvalidDataException for signaling defective XML

--- src/Injector/Solver/Selections.cs
+++ src/Injector/Solver/Selections.cs
@@ -172,7 +172,7 @@ namespace ZeroInstall.Injector.Solver
         /// <exception cref="KeyNotFoundException">Thrown if the requested feed was not found in the cache.</exception>
         /// <exception cref="IOException">Thrown if a problem occured while reading the feed file.</exception>
         /// <exception cref="UnauthorizedAccessException">Thrown if read access to the cache is not permitted.</exception>
-        /// <exception cref="InvalidOperationException">Thrown if the feed file could not be parsed.</exception>
+        /// <exception cref="InvalidDataException">Thrown if the feed file could not be parsed.</exception>
         public IEnumerable<Implementation> ListUncachedImplementations(IStore searchStore, IFeedCache feedCache)
         {
             #region Sanity checks
