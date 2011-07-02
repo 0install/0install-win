217d61c31d357840f86cf69609579bf1dd1f2d3a
Bastian Eicher <bastian@eicher.net>
2011-07-02 16:44:14 +0000

    Added missing localization for SelectionsControl

--- src/Commands.WinForms/SelectionsControl.Designer.cs
+++ src/Commands.WinForms/SelectionsControl.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectionsControl));
             this.tableLayout = new System.Windows.Forms.TableLayoutPanel();
             this.labelName = new System.Windows.Forms.Label();
             this.labelVersion = new System.Windows.Forms.Label();
@@ -40,82 +41,47 @@
             // 
             // tableLayout
             // 
-            this.tableLayout.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayout.AutoSize = true;
-            this.tableLayout.ColumnCount = 3;
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            resources.ApplyResources(this.tableLayout, "tableLayout");
             this.tableLayout.Controls.Add(this.labelName, 0, 0);
             this.tableLayout.Controls.Add(this.labelVersion, 1, 0);
             this.tableLayout.Controls.Add(this.labelTask, 2, 0);
-            this.tableLayout.Location = new System.Drawing.Point(0, 0);
             this.tableLayout.Name = "tableLayout";
-            this.tableLayout.RowCount = 1;
-            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayout.Size = new System.Drawing.Size(350, 19);
-            this.tableLayout.TabIndex = 0;
             // 
             // labelName
             // 
-            this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(3, 0);
+            resources.ApplyResources(this.labelName, "labelName");
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(35, 13);
-            this.labelName.TabIndex = 0;
-            this.labelName.Text = "Name";
             // 
             // labelVersion
             // 
-            this.labelVersion.AutoSize = true;
-            this.labelVersion.Location = new System.Drawing.Point(90, 0);
+            resources.ApplyResources(this.labelVersion, "labelVersion");
             this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(42, 13);
-            this.labelVersion.TabIndex = 1;
-            this.labelVersion.Text = "Version";
             // 
             // labelTask
             // 
-            this.labelTask.AutoSize = true;
-            this.labelTask.Location = new System.Drawing.Point(150, 0);
+            resources.ApplyResources(this.labelTask, "labelTask");
             this.labelTask.Name = "labelTask";
-            this.labelTask.Size = new System.Drawing.Size(31, 13);
-            this.labelTask.TabIndex = 2;
-            this.labelTask.Text = "Task";
             // 
             // buttonContinue
             // 
-            this.buttonContinue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonContinue.Location = new System.Drawing.Point(3, 32);
+            resources.ApplyResources(this.buttonContinue, "buttonContinue");
             this.buttonContinue.Name = "buttonContinue";
-            this.buttonContinue.Size = new System.Drawing.Size(75, 23);
-            this.buttonContinue.TabIndex = 1;
-            this.buttonContinue.Text = "Continue";
             this.buttonContinue.UseVisualStyleBackColor = true;
-            this.buttonContinue.Visible = false;
             this.buttonContinue.Click += new System.EventHandler(this.buttonContinue_Click);
             // 
             // scrollPanel
             // 
-            this.scrollPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.scrollPanel.AutoScroll = true;
+            resources.ApplyResources(this.scrollPanel, "scrollPanel");
             this.scrollPanel.Controls.Add(this.tableLayout);
-            this.scrollPanel.Location = new System.Drawing.Point(0, 0);
             this.scrollPanel.Name = "scrollPanel";
-            this.scrollPanel.Size = new System.Drawing.Size(350, 26);
-            this.scrollPanel.TabIndex = 0;
             // 
             // SelectionsControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.buttonContinue);
             this.Controls.Add(this.scrollPanel);
             this.Name = "SelectionsControl";
-            this.Size = new System.Drawing.Size(350, 55);
             this.tableLayout.ResumeLayout(false);
             this.tableLayout.PerformLayout();
             this.scrollPanel.ResumeLayout(false);
