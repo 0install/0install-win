39038f8c0021516743ff936182ffeef4e37f439b
Bastian Eicher <bastian@eicher.net>
2011-09-04 13:54:53 +0000

    Added location-specific mutex even in debug mode

--- src/Store.Management.WinForms/Program.cs
+++ src/Store.Management.WinForms/Program.cs
@@ -50,11 +50,11 @@ namespace ZeroInstall.Store.Management.WinForms
         {
             WindowsUtils.SetCurrentProcessAppID(AppUserModelID);
 
-#if !DEBUG
             // Prevent launch during update and allow instance detection
             string mutexName = "mutex-" + StringUtils.Hash(Locations.InstallBase, SHA256.Create());
             if (AppMutex.Probe(mutexName + "-update")) return;
             AppMutex.Create(mutexName);
+#if !DEBUG
             AppMutex.Create("Zero Install");
 #endif
 
