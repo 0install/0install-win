84db83778e63f19c461ed412892f1455e37c5a3f
Bastian Eicher <bastian@eicher.net>
2011-11-20 16:17:35 +0000

    Incremented version number to 1.4

--- setup/setup.iss
+++ setup/setup.iss
@@ -1,5 +1,5 @@
 ;Version numbers
-#define Version "1.1.2"
+#define Version "1.4"
 
 ;Automatic dependency download and installation
 #include "scripts\fileversion.iss"
--- src/AssemblyInfo.Global.cs
+++ src/AssemblyInfo.Global.cs
@@ -6,5 +6,5 @@
 [assembly: AssemblyCopyright("Copyright Bastian Eicher et al. 2010-2011")]
 
 // Version information
-[assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyFileVersion("1.1.2")]
+[assembly: AssemblyVersion("1.4")]
+[assembly: AssemblyFileVersion("1.4")]
