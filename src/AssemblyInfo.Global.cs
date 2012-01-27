878d3ee40e1bb484113a4a00e4a2911384ac2484
Bastian Eicher <bastian@eicher.net>
2012-01-27 12:16:04 +0000

    Incremented version number to 1.5.1

--- src/AssemblyInfo.Global.cs
+++ src/AssemblyInfo.Global.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 
 // Version information
 
-[assembly: AssemblyVersion("1.5")]
-[assembly: AssemblyFileVersion("1.5")]
+[assembly: AssemblyVersion("1.5.1")]
+[assembly: AssemblyFileVersion("1.5.1")]
