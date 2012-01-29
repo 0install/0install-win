1530274b634ded72decc150114b5ed3d58678aad
Bastian Eicher <bastian@eicher.net>
2012-01-29 21:59:26 +0000

    Added missing copyright comments

--- src/Central.WinForms/SyncConfig/SyncCredentials.cs
+++ src/Central.WinForms/SyncConfig/SyncCredentials.cs
@@ -1,5 +1,25 @@
-﻿namespace ZeroInstall.Central.WinForms.SyncConfig
+﻿/*
+ * Copyright 2010-2012 Bastian Eicher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace ZeroInstall.Central.WinForms.SyncConfig
 {
+    /// <summary>
+    /// Stores login credentials for a Sync server.
+    /// </summary>
     internal struct SyncCredentials
     {
         public string Username { get; private set; }
