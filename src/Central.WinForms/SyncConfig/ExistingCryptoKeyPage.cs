46a3a0541781c0a4a429c4968e93dfbe002fdc7c
Bastian Eicher <bastian@eicher.net>
2012-03-17 15:55:45 +0000

    Added 10 seconds timeout to Sync Setup wizard's crypto key check

--- src/Central.WinForms/SyncConfig/ExistingCryptoKeyPage.cs
+++ src/Central.WinForms/SyncConfig/ExistingCryptoKeyPage.cs
@@ -32,6 +32,25 @@ namespace ZeroInstall.Central.WinForms.SyncConfig
 {
     internal partial class ExistingCryptoKeyPage : UserControl
     {
+        #region Inner class
+        private class WebClientTimeout : WebClient
+        {
+            private readonly int _timeout;
+
+            public WebClientTimeout(int timeout)
+            {
+                _timeout = timeout;
+            }
+            
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                var result = base.GetWebRequest(address);
+                if (result != null) result.Timeout = _timeout;
+                return result;
+            }
+        }
+        #endregion
+
         public Uri SyncServer;
         public SyncCredentials SyncCredentials;
 
@@ -77,10 +96,10 @@ namespace ZeroInstall.Central.WinForms.SyncConfig
             if (!syncServer.ToString().EndsWith("/")) syncServer = new Uri(syncServer + "/"); // Ensure the server URI references a directory
             var appListUri = new Uri(syncServer, new Uri("app-list", UriKind.Relative));
 
-            using (var webClient = new WebClient
+            using (var webClient = new WebClientTimeout(10000)
             {
                 Credentials = new NetworkCredential(syncCredentials.Username, syncCredentials.Password),
-                CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore),
+                CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore)
             })
             {
                 try
