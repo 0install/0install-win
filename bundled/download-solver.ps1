86def161758bff068e91c9e8350822cc665126f5
Bastian Eicher <bastian@eicher.net>
2012-08-29 16:12:47 +0000

    Modified solver download script to use SharpZipLib instead of explorer shell for ZIP extraction

--- bundled/download-solver.ps1
+++ bundled/download-solver.ps1
@@ -1,7 +1,7 @@
 # This script will download and extract the external solver to the current directory
 
 $sourceUrl = "http://0install.de/files/zero-install-solver.zip"
-$tempFile = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "zero-install-solver.zip")
+$tempFile = [System.IO.Path]::GetTempFileName()
 $targetDir = (get-location).Path
 
 echo "Downloading external solver..."
@@ -9,7 +9,6 @@ $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($sourceUrl, $tempFile)
 
 echo "Extracting external solver..."
-$shellApp = New-Object -com shell.application
-$zipFile = $shellApp.namespace($tempFile)
-$targetFolder = $shellApp.namespace($targetDir)
-$targetFolder.Copyhere($zipFile.items(), 4+16+1024)
+$null = [Reflection.Assembly]::LoadFile($targetDir + "\..\lib\ICSharpCode.SharpZipLib.dll")
+$fastZip = New-Object ICSharpCode.SharpZipLib.Zip.FastZip
+$fastZip.ExtractZip($tempFile, $targetDir, "")
