2b413cf8f29160ea25da69621921888091c594bd
Bastian Eicher <bastian@eicher.net>
2012-09-07 13:55:30 +0000

    Improved TeamCity integration and version handling

--- version.cmd
+++ version.cmd
@@ -6,3 +6,9 @@ set /p version_tools= < "%~dp0version-tools"
 set /p version_updater= < "%~dp0version-updater"
 
 echo ##teamcity[buildNumber '%version%_{build.number}']
+echo ##teamcity[setParameter name='build.version' value='%version%']
+echo ##teamcity[publishArtifacts 'version']
+echo ##teamcity[setParameter name='build.version_tools' value='%version_tools%']
+echo ##teamcity[publishArtifacts 'version_tools']
+echo ##teamcity[setParameter name='build.version_updater' value='%version_updater%']
+echo ##teamcity[publishArtifacts 'version_updater']
--- version.sh
+++ version.sh
@@ -7,3 +7,9 @@ export VERSION_TOOLS=`cat version-tools`
 export VERSION_UPDATER=`cat version-updater`
 
 echo \#\#teamcity[buildNumber \'$VERSION\_{build.number}\']
+echo \#\#teamcity[setParameter name=\'build.version\' value=\'$VERSION\']
+echo \#\#teamcity[publishArtifacts \'version\']
+echo \#\#teamcity[setParameter name=\'build.version_tools\' value=\'$VERSION_TOOLS\']
+echo \#\#teamcity[publishArtifacts \'version_tools\']
+echo \#\#teamcity[setParameter name=\'build.version_updater\' value=\'$VERSION_UPDATER\']
+echo \#\#teamcity[publishArtifacts \'version_updater\']
