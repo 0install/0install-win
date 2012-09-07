ce0ee5c4cf76615b5ee2d50ecfbaec52b791e7ce
Bastian Eicher <bastian@eicher.net>
2012-09-07 11:10:37 +0000

    Added TeamCity messages to Setup build script

--- setup/build.cmd
+++ setup/build.cmd
@@ -21,11 +21,12 @@ set /p version_updater= < "%~dp0version-updater"
 copy "%~dp0version" "%TargetDir%\version" > NUL
 copy "%~dp0version-tools" "%TargetDir%\version-tools" > NUL
 if "%BUILD_UPDATER%"=="TRUE" copy "%~dp0version_updater" "%TargetDir%\version_updater" > NUL
+echo ##teamcity[buildNumber '%version%_{build.number}']
 
 rem Use bundled utility EXEs
 path %~dp0utils;%path%
 
-echo Building ZIP archive...
+echo ##teamcity[progressMessage 'Building ZIP archive']
 cd /d "%~dp0..\bundled"
 zip -q -9 -r "%TargetDir%\zero-install.zip" GnuPG Solver
 if errorlevel 1 pause
@@ -37,16 +38,19 @@ if errorlevel 1 pause
 zip -q -9 -j "%TargetDir%\zero-install.zip" "%~dp0..\3rd party code.txt"
 if errorlevel 1 pause
 cd /d "%~dp0"
+echo ##teamcity[publishArtifacts 'build/Setup/zero-install.zip']
 
-echo Building TAR.BZ2 archive...
+echo ##teamcity[progressMessage 'Building TAR.BZ2 archive']
 bsdtar -cjf "%TargetDir%\zero-install-%version%.tar.bz2" --exclude=*.log --exclude=*.mdb --exclude=*.vshost.exe --exclude=Test.* --exclude=nunit.* --exclude=Mono.* --exclude=*.pdb --exclude=*.xml -C "%~dp0.." "license.txt" -C "%~dp0.." "3rd party code.txt" -C "%~dp0..\bundled" GnuPG Solver -C "%~dp0..\build\Frontend\Release" .
 if errorlevel 1 pause
+echo ##teamcity[publishArtifacts 'build/Setup/zero-install-%version%.tar.bz2']
 
-echo Building Tools archive...
+echo ##teamcity[progressMessage 'Building Tools archive']
 bsdtar -cjf "%TargetDir%\zero-install-tools-%version_tools%.tar.bz2" --exclude=*.log --exclude=*.mdb --exclude=*.vshost.exe --exclude=Test.* --exclude=nunit.* --exclude=Mono.* --exclude=*.pdb --exclude=*.xml -C "%~dp0.." "license.txt" -C "%~dp0.." "3rd party code.txt" -C "%~dp0..\build\Tools\Release" .
 if errorlevel 1 pause
+echo ##teamcity[publishArtifacts 'build/Setup/zero-install-tools-%version_tools%.tar.bz2']
 
-echo Building Tools developer archive...
+echo ##teamcity[progressMessage 'Building Tools developer archive']
 cd /d "%~dp0..\bundled"
 zip -q -9 -r "%TargetDir%\zero-install-tools-dev.zip" GnuPG
 cd /d "%~dp0..\build\Tools\Release"
@@ -57,8 +61,9 @@ if errorlevel 1 pause
 zip -q -9 -j "%TargetDir%\zero-install-tools-dev.zip" "%~dp0..\3rd party code.txt"
 if errorlevel 1 pause
 cd /d "%~dp0"
+echo ##teamcity[publishArtifacts 'build/Setup/zero-install-tools-dev.zip']
 
-echo Building Backend developer archive...
+echo ##teamcity[progressMessage 'Building Backend developer archive']
 cd /d "%~dp0..\bundled"
 zip -q -9 -r "%TargetDir%\zero-install-backend-dev.zip" GnuPG Solver
 cd /d "%~dp0..\build\Backend\Release"
@@ -69,11 +74,13 @@ if errorlevel 1 pause
 zip -q -9 -j "%TargetDir%\zero-install-backend-dev.zip" "%~dp0..\3rd party code.txt"
 if errorlevel 1 pause
 cd /d "%~dp0"
+echo ##teamcity[publishArtifacts 'build/Setup/zero-install-backend-dev.zip']
 
 if "%BUILD_UPDATER%"=="TRUE" (
-  echo Building Updater archive...
+  echo ##teamcity[progressMessage 'Building Updater archive']
   bsdtar -cjf "%TargetDir%\zero-install-updater-%version_updater%.tar.bz2" --exclude=*.log --exclude=*.pdb --exclude=*.mdb --exclude=*.vshost.exe --exclude=Test.* --exclude=nunit.* --exclude=Mono.* --exclude=*.xml --exclude=SevenZip.* --exclude=C5.* -C "%~dp0..\build\Updater\Release" .
   if errorlevel 1 pause
+  echo ##teamcity[publishArtifacts 'build/Setup/zero-install-updater-%version_updater%.tar.bz2']
 )
 
 rem Handle WOW
@@ -87,10 +94,11 @@ if not exist "%ProgramFiles_temp%\Inno Setup 5" (
   goto end
 )
 
-echo Building installer...
+echo ##teamcity[progressMessage 'Building installer']
 cd /d "%~dp0"
 "%ProgramFiles_temp%\Inno Setup 5\iscc.exe" /q "/dVersion=%version%" setup.iss
 if errorlevel 1 pause
+echo ##teamcity[publishArtifacts 'build/Setup/zero-install.exe']
 
 if "%1"=="+run" "%TargetDir%\zero-install.exe" /silent
 if "%2"=="+run" "%TargetDir%\zero-install.exe" /silent
