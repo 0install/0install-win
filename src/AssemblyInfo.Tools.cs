04f2ec1c27c7dd83aa2576447d622ef8e955af27
Bastian Eicher <bastian@eicher.net>
2012-09-12 16:53:34 +0000

    Incremented version number to 1.11.1

--- src/AssemblyInfo.Tools.cs
+++ src/AssemblyInfo.Tools.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 
 // Version information
 
-[assembly: AssemblyVersion("0.9.0")]
-[assembly: AssemblyFileVersion("0.9.0")]
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyFileVersion("0.9.1")]
