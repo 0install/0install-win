4f34a45eaa02996da092366c4feef297c087c046
Bastian Eicher <bastian@eicher.net>
2012-09-19 11:36:13 +0000

    Exclude .svn directory from auto-built archives

--- setup/build.cmd
+++ setup/build.cmd
@@ -27,10 +27,10 @@ path %~dp0utils;%path%
 
 echo ##teamcity[progressMessage 'Building ZIP archive']
 cd /d "%~dp0..\bundled"
-zip -q -9 -r "%TargetDir%\zero-install.zip" GnuPG Solver
+zip -q -9 -r "%TargetDir%\zero-install.zip" GnuPG Solver --exclude .svn
 if errorlevel 1 pause
 cd /d "%~dp0..\build\Frontend\Release"
-zip -q -9 -r "%TargetDir%\zero-install.zip" . --exclude *.log *.mdb *.vshost.exe Test.* nunit.* Mono.* *.pdb *.xml
+zip -q -9 -r "%TargetDir%\zero-install.zip" . --exclude .svn *.log *.mdb *.vshost.exe Test.* nunit.* Mono.* *.pdb *.xml
 if errorlevel 1 pause
 zip -q -9 -j "%TargetDir%\zero-install.zip" "%~dp0..\license.txt"
 if errorlevel 1 pause
@@ -40,12 +40,12 @@ cd /d "%~dp0"
 echo ##teamcity[publishArtifacts 'build/Setup/zero-install.zip']
 
 echo ##teamcity[progressMessage 'Building TAR.BZ2 archive']
-bsdtar -cjf "%TargetDir%\zero-install-%version%.tar.bz2" --exclude=*.log --exclude=*.mdb --exclude=*.vshost.exe --exclude=Test.* --exclude=nunit.* --exclude=Mono.* --exclude=*.pdb --exclude=*.xml -C "%~dp0.." "license.txt" -C "%~dp0.." "3rd party code.txt" -C "%~dp0..\bundled" GnuPG Solver -C "%~dp0..\build\Frontend\Release" .
+bsdtar -cjf "%TargetDir%\zero-install-%version%.tar.bz2" --exclude=.svn --exclude=*.log --exclude=*.mdb --exclude=*.vshost.exe --exclude=Test.* --exclude=nunit.* --exclude=Mono.* --exclude=*.pdb --exclude=*.xml -C "%~dp0.." "license.txt" -C "%~dp0.." "3rd party code.txt" -C "%~dp0..\bundled" GnuPG Solver -C "%~dp0..\build\Frontend\Release" .
 if errorlevel 1 pause
 echo ##teamcity[publishArtifacts 'build/Setup/zero-install-%version%.tar.bz2']
 
 echo ##teamcity[progressMessage 'Building Tools archive']
-bsdtar -cjf "%TargetDir%\zero-install-tools-%version_tools%.tar.bz2" --exclude=*.log --exclude=*.mdb --exclude=*.vshost.exe --exclude=Test.* --exclude=nunit.* --exclude=Mono.* --exclude=*.pdb --exclude=*.xml -C "%~dp0.." "license.txt" -C "%~dp0.." "3rd party code.txt" -C "%~dp0..\build\Tools\Release" .
+bsdtar -cjf "%TargetDir%\zero-install-tools-%version_tools%.tar.bz2" --exclude=.svn --exclude=*.log --exclude=*.mdb --exclude=*.vshost.exe --exclude=Test.* --exclude=nunit.* --exclude=Mono.* --exclude=*.pdb --exclude=*.xml -C "%~dp0.." "license.txt" -C "%~dp0.." "3rd party code.txt" -C "%~dp0..\build\Tools\Release" .
 if errorlevel 1 pause
 echo ##teamcity[publishArtifacts 'build/Setup/zero-install-tools-%version_tools%.tar.bz2']
 
@@ -53,7 +53,7 @@ echo ##teamcity[progressMessage 'Building Tools developer archive']
 cd /d "%~dp0..\bundled"
 zip -q -9 -r "%TargetDir%\zero-install-tools-dev.zip" GnuPG
 cd /d "%~dp0..\build\Tools\Release"
-zip -q -9 -r "%TargetDir%\zero-install-tools-dev.zip" . --exclude *.log *.mdb *.vshost.exe Test.* nunit.* Mono.*
+zip -q -9 -r "%TargetDir%\zero-install-tools-dev.zip" . --exclude=.svn --exclude *.log *.mdb *.vshost.exe Test.* nunit.* Mono.*
 if errorlevel 1 pause
 zip -q -9 -j "%TargetDir%\zero-install-tools-dev.zip" "%~dp0..\license.txt"
 if errorlevel 1 pause
@@ -66,7 +66,7 @@ echo ##teamcity[progressMessage 'Building Backend developer archive']
 cd /d "%~dp0..\bundled"
 zip -q -9 -r "%TargetDir%\zero-install-backend-dev.zip" GnuPG Solver
 cd /d "%~dp0..\build\Backend\Release"
-zip -q -9 -r "%TargetDir%\zero-install-backend-dev.zip" . --exclude *.log *.mdb *.vshost.exe Test.* nunit.* Mono.*
+zip -q -9 -r "%TargetDir%\zero-install-backend-dev.zip" . --exclude .svn *.log *.mdb *.vshost.exe Test.* nunit.* Mono.*
 if errorlevel 1 pause
 zip -q -9 -j "%TargetDir%\zero-install-backend-dev.zip" "%~dp0..\license.txt"
 if errorlevel 1 pause
@@ -77,7 +77,7 @@ echo ##teamcity[publishArtifacts 'build/Setup/zero-install-backend-dev.zip']
 
 if "%BUILD_UPDATER%"=="TRUE" (
   echo ##teamcity[progressMessage 'Building Updater archive']
-  bsdtar -cjf "%TargetDir%\zero-install-updater-%version_updater%.tar.bz2" --exclude=*.log --exclude=*.pdb --exclude=*.mdb --exclude=*.vshost.exe --exclude=Test.* --exclude=nunit.* --exclude=Mono.* --exclude=*.xml --exclude=SevenZip.* --exclude=C5.* -C "%~dp0..\build\Updater\Release" .
+  bsdtar -cjf "%TargetDir%\zero-install-updater-%version_updater%.tar.bz2" --exclude=.svn --exclude=*.log --exclude=*.pdb --exclude=*.mdb --exclude=*.vshost.exe --exclude=Test.* --exclude=nunit.* --exclude=Mono.* --exclude=*.xml --exclude=SevenZip.* --exclude=C5.* -C "%~dp0..\build\Updater\Release" .
   if errorlevel 1 pause
   echo ##teamcity[publishArtifacts 'build/Setup/zero-install-updater-%version_updater%.tar.bz2']
 )
