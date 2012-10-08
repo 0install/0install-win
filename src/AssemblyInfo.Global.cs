7d49fb706d12343d22c485a7db98945ef7476c9c
Bastian Eicher <bastian@eicher.net>
2012-10-08 09:53:04 +0000

    Incremented version numbers

--- src/AssemblyInfo.Global.cs
+++ src/AssemblyInfo.Global.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 
 // Version information
 
-[assembly: AssemblyVersion("1.12.1")]
-[assembly: AssemblyFileVersion("1.12.1")]
+[assembly: AssemblyVersion("1.12.2")]
+[assembly: AssemblyFileVersion("1.12.2")]
