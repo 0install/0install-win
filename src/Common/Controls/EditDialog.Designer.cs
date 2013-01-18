ec4faba53356580caa39e4d68e420486cf4a61f2
Bastian Eicher <bastian@eicher.net>
2013-01-18 17:17:02 +0000

    Made EditDialog layout DPI-resistant

--- src/Common/Controls/EditDialog.Designer.cs
+++ src/Common/Controls/EditDialog.Designer.cs
@@ -33,6 +33,14 @@
             this.buttonResetValue = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(146, 256);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(227, 256);
+            // 
             // propertyGrid
             // 
             this.propertyGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -40,7 +48,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.propertyGrid.Location = new System.Drawing.Point(12, 12);
             this.propertyGrid.Name = "propertyGrid";
-            this.propertyGrid.Size = new System.Drawing.Size(260, 209);
+            this.propertyGrid.Size = new System.Drawing.Size(290, 238);
             this.propertyGrid.TabIndex = 0;
             this.propertyGrid.ToolbarVisible = false;
             this.propertyGrid.SelectedGridItemChanged += new System.Windows.Forms.SelectedGridItemChangedEventHandler(this.propertyGrid_SelectedGridItemChanged);
@@ -48,9 +56,9 @@
             // buttonResetValue
             // 
             this.buttonResetValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonResetValue.Location = new System.Drawing.Point(12, 227);
+            this.buttonResetValue.Location = new System.Drawing.Point(12, 256);
             this.buttonResetValue.Name = "buttonResetValue";
-            this.buttonResetValue.Size = new System.Drawing.Size(98, 23);
+            this.buttonResetValue.Size = new System.Drawing.Size(114, 23);
             this.buttonResetValue.TabIndex = 999;
             this.buttonResetValue.Text = "(Reset value)";
             this.buttonResetValue.UseVisualStyleBackColor = true;
@@ -58,11 +66,17 @@
             // 
             // EditDialog
             // 
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(314, 291);
             this.Controls.Add(this.propertyGrid);
             this.Controls.Add(this.buttonResetValue);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            this.MinimumSize = new System.Drawing.Size(320, 200);
             this.Name = "EditDialog";
+            this.Controls.SetChildIndex(this.buttonResetValue, 0);
+            this.Controls.SetChildIndex(this.propertyGrid, 0);
+            this.Controls.SetChildIndex(this.buttonOK, 0);
+            this.Controls.SetChildIndex(this.buttonCancel, 0);
             this.ResumeLayout(false);
 
         }
