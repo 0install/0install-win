f94730a88b0967cc5ed7a67b9a18b55f2ad8fcb6
Bastian Eicher <bastian@eicher.net>
2013-04-04 17:12:04 +0000

    Incremented Updater version

--- src/AssemblyInfo.Updater.cs
+++ src/AssemblyInfo.Updater.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 
 // Version information
 
-[assembly: AssemblyVersion("1.2.6")]
-[assembly: AssemblyFileVersion("1.2.6")]
+[assembly: AssemblyVersion("1.2.7")]
+[assembly: AssemblyFileVersion("1.2.7")]
