0274196265fd69816f3f470bd4ff059c69ffd4b1
Bastian Eicher <bastian@eicher.net>
2013-04-17 00:52:49 +0000

    Added SharpDevelop solution with Boo and IronPython samples

---
+++ src/Sample.Boo/MinimalZeroInstall.boo
@@ -0,0 +1,13 @@
+﻿import ZeroInstall.Model
+import ZeroInstall.Injector
+import ZeroInstall.Injector.Solver
+ 
+def Main(args as (string)):
+    run(Requirements(InterfaceID:args[0]))
+ 
+def run(requirements):
+    policy = Policy.CreateDefault(CliHandler())
+    selections = policy.Solve(requirements)
+    missing = SelectionsUtils.GetUncachedImplementations(selections, policy)
+    policy.Fetch(missing)
+    Executor(selections, policy.Fetcher.Store).Start()
---
+++ src/Sample.IPython/MinimalZeroInstall.py
@@ -0,0 +1,16 @@
+﻿#import clr
+#clr.AddReferenceToFile("ZeroInstall.Model.dll", "ZeroInstall.Injector.dll")
+
+from ZeroInstall.Model import Architecture, Requirements
+from ZeroInstall.Injector import Policy, CliHandler, Executor
+from ZeroInstall.Injector.Solver import SelectionsUtils
+
+def run(requirements):
+    policy = Policy.CreateDefault(CliHandler())
+    selections = policy.Solve(requirements)
+    missing = SelectionsUtils.GetUncachedImplementations(selections, policy)
+    policy.Fetch(missing)
+    Executor(selections, policy.Fetcher.Store).Start()
+
+import sys
+run(Requirements(InterfaceID = sys.argv[0]))
