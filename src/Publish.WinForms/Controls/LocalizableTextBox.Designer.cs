a7f5a4c7f6d161831218a1342456454da9d85a39
Bastian Eicher <bastian@eicher.net>
2013-06-22 22:44:37 +0000

    Started redesigning LocalizableTextControl into LocalizableTextBox

---
+++ src/Publish.WinForms/Controls/LocalizableTextBox.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ZeroInstall.Publish.WinForms.Controls
+{
+    partial class LocalizableTextBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being Used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new Common.Controls.HintTextBox();
+            this.comboBoxLanguage = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.AccessibleName = "";
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.HintText = "";
+            this.textBox.Location = new System.Drawing.Point(85, 2);
+            this.textBox.Name = "textBox";
+            this.textBox.ShowClearButton = true;
+            this.textBox.Size = new System.Drawing.Size(342, 20);
+            this.textBox.TabIndex = 10;
+            this.textBox.Validating += new System.ComponentModel.CancelEventHandler(this.textBox_Validating);
+            // 
+            // comboBoxLanguage
+            // 
+            this.comboBoxLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxLanguage.FormattingEnabled = true;
+            this.comboBoxLanguage.Location = new System.Drawing.Point(0, 1);
+            this.comboBoxLanguage.Name = "comboBoxLanguage";
+            this.comboBoxLanguage.Size = new System.Drawing.Size(79, 21);
+            this.comboBoxLanguage.TabIndex = 0;
+            // 
+            // LocalizableTextBox
+            // 
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.comboBoxLanguage);
+            this.Name = "LocalizableTextBox";
+            this.Size = new System.Drawing.Size(427, 23);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxLanguage;
+        public Common.Controls.HintTextBox textBox;
+    }
+}
---
+++ src/Publish.WinForms/Controls/LocalizableTextBox.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Copyright 2011-2012 Simon E. Silva Lauinger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+using Common.Collections;
+using Common.Controls;
+using Common.Undo;
+
+namespace ZeroInstall.Publish.WinForms.Controls
+{
+    /// <summary>
+    /// A control for editing a <see cref="LocalizableStringCollection"/>.
+    /// </summary>
+    public partial class LocalizableTextBox : UserControl, IEditorControl<LocalizableStringCollection>
+    {
+        #region Properties
+        private LocalizableStringCollection _target;
+
+        /// <inheritdoc/>
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public LocalizableStringCollection Target
+        {
+            get { return _target; }
+            set
+            {
+                _target = value;
+                FillComboBoxLanguages();
+            }
+        }
+
+        /// <inheritdoc/>
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Common.Undo.ICommandExecutor CommandExecutor { get; set; }
+        #endregion
+
+        public LocalizableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly CultureInfo[] _cultures = CultureInfo.GetCultures(CultureTypes.FrameworkCultures);
+
+        private void FillComboBoxLanguages()
+        {
+            var languages = _cultures.Select(culture => new Language {Culture = culture, IsValueSet = Target.ContainsExactLanguage(culture)});
+
+            comboBoxLanguage.BeginUpdate();
+            comboBoxLanguage.DataSource = languages;
+            comboBoxLanguage.EndUpdate();
+        }
+
+        private class Language
+        {
+            public CultureInfo Culture;
+            public bool IsValueSet;
+
+            public override string ToString()
+            {
+                return IsValueSet
+                    ? "* " + Culture.IetfLanguageTag
+                    : Culture.IetfLanguageTag;
+            }
+        }
+
+        private void textBox_Validating(object sender, CancelEventArgs e)
+        {
+            var language = comboBoxLanguage.SelectedItem as Language;
+            if (language == null) return;
+
+            var command = new SetLocalizableString(Target, new LocalizableString(textBox.Text, language.Culture));
+            CommandExecutor.Execute(command);
+        }
+
+        public override void Refresh()
+        {
+            FillComboBoxLanguages();
+            base.Refresh();
+        }
+    }
+}
