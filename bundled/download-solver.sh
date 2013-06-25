8b960d428ebf4e47d92b6daaa4beb6ccdfd5f7c2
Bastian Eicher <bastian@eicher.net>
2013-06-25 18:22:15 +0000

    Updated bundled Solver download script

--- bundled/download-solver.sh
+++ bundled/download-solver.sh
@@ -2,7 +2,7 @@
 #This script will download and extract the external solver to the current directory
 
 echo "Downloading external solver..."
-wget -q --no-check-certificate https://0install.de/teamcity/guestAuth/repository/downloadAll/bt29/.lastSuccessful/artifacts.zip
+wget -q --no-check-certificate https://0install.de/teamcity/guestAuth/repository/downloadAll/ZeroInstall_CSharp_PythonSolver/.lastSuccessful/artifacts.zip
 
 echo "Extracting external solver..."
 unzip -qo artifacts.zip
