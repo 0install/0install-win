90850218371859a712c5dafedc77cdf470b64a29
Bastian Eicher <bastian@eicher.net>
2013-08-29 03:23:22 +0200

    Made "internal" members visible to Unit Tests

--- src/Solvers/Properties/AssemblyInfo.cs
+++ src/Solvers/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Assembly info
@@ -12,6 +13,7 @@ using System.Runtime.InteropServices;
 // Security settings
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("Test.Backend, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a567d17b1bb41a344be5d64a3738f897270b036bec653e453b8e350e0d1b640ee9405fc6903fa80cc56b325a861ee3c20055cdbc3821107e1adb8d6c8743c5f089ef140704d93091fa4a65e2cba3ac278b68b2c4403a509c65505722eaff5e972658a496dd944d971a6da3056b4f21aaf777bf76c03c19a81be49762a4cb28af")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8b3651ad-3221-4eb0-a36d-19cd19aba065")]
