938a593e70ed99c8ec1eb27aa5d9b48a29e12338
Bastian Eicher <bastian@eicher.net>
2013-09-15 15:12:54 +0200

    Improved unit tests

---
+++ src/Test.Frontend/Commands/CommandFactoryTest.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Copyright 2010-2013 Bastian Eicher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using NDesk.Options;
+using NUnit.Framework;
+using ZeroInstall.Backend;
+
+namespace ZeroInstall.Commands
+{
+    /// <summary>
+    /// Contains test methods for <see cref="CommandFactory"/>.
+    /// </summary>
+    [TestFixture]
+    public class CommandFactoryTest
+    {
+        /// <summary>
+        /// Ensures <see cref="CommandFactory.CreateAndParse"/> correctly parses command names in argument lists.
+        /// </summary>
+        [Test]
+        public void TestCreateCommand()
+        {
+            foreach (string name in CommandFactory.CommandNames)
+            {
+                var command = CreateCommand(name);
+                Assert.AreEqual(name, command.Name);
+            }
+        }
+
+        private static FrontendCommand CreateCommand(string name)
+        {
+            var handler = new SilentHandler();
+            try
+            {
+                return CommandFactory.CreateAndParse(new[] {name}, handler);
+            }
+            catch (OptionException)
+            {
+                // Pass in an additional dummy argument if required
+                return CommandFactory.CreateAndParse(new[] {name, "dummy"}, handler);
+            }
+        }
+    }
+}
