6f7bdca1161b4efd3b865be927a5660d92e1399a
Bastian Eicher <bastian@eicher.net>
2013-11-06 14:01:32 +0100

    Incremented Tools version number

--- src/AssemblyInfo.Tools.cs
+++ src/AssemblyInfo.Tools.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright 2010-2013 Bastian Eicher")]
 
 // Version information
-[assembly: AssemblyVersion("1.0.5")]
-[assembly: AssemblyFileVersion("1.0.5")]
+[assembly: AssemblyVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6")]
