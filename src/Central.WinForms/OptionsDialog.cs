65eb6a26fd5fae573d123a88c92ba52afd16f944
Bastian Eicher <bastian@eicher.net>
2013-12-17 01:04:48 +0100

    Central.WinForms: Improved error handling OptionsDialog

--- src/Central.WinForms/OptionsDialog.cs
+++ src/Central.WinForms/OptionsDialog.cs
@@ -51,7 +51,27 @@ namespace ZeroInstall.Central.WinForms
 
         private void OptionsDialog_Load(object sender, EventArgs e)
         {
-            LoadConfig();
+            try
+            {
+                LoadConfig();
+            }
+                #region Error handling
+            catch (IOException ex)
+            {
+                Msg.Inform(this, Resources.ProblemLoadingOptions + "\n" + ex.Message, MsgSeverity.Error);
+                Close();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Msg.Inform(this, Resources.ProblemLoadingOptions + "\n" + ex.Message, MsgSeverity.Error);
+                Close();
+            }
+            catch (InvalidDataException ex)
+            {
+                Msg.Inform(this, Resources.ProblemLoadingOptions + "\n" + ex.Message + (ex.InnerException == null ? "" : "\n" + ex.InnerException.Message), MsgSeverity.Error);
+                Close();
+            }
+            #endregion
         }
         #endregion
 
@@ -61,109 +81,75 @@ namespace ZeroInstall.Central.WinForms
 
         private void LoadConfig()
         {
-            try
+            // Fill fields with data from config
+            var config = Config.Load();
+            switch (config.NetworkUse)
             {
-                // Fill fields with data from config
-                var config = Config.Load();
-                switch (config.NetworkUse)
-                {
-                    case NetworkLevel.Full:
-                        radioNetworkUseFull.Checked = true;
-                        break;
-                    case NetworkLevel.Minimal:
-                        radioNetworkUseMinimal.Checked = true;
-                        break;
-                    case NetworkLevel.Offline:
-                        radioNetworkUseOffline.Checked = true;
-                        break;
-                }
-                checkBoxHelpWithTesting.Checked = config.HelpWithTesting;
-                checkBoxAutoApproveKeys.Checked = config.AutoApproveKeys;
-                textBoxSyncServer.Uri = config.SyncServer;
-                textBoxSyncUsername.Text = config.SyncServerUsername;
-                textBoxSyncPassword.Text = config.SyncServerPassword;
-                textBoxSyncCryptoKey.Text = config.SyncCryptoKey;
+                case NetworkLevel.Full:
+                    radioNetworkUseFull.Checked = true;
+                    break;
+                case NetworkLevel.Minimal:
+                    radioNetworkUseMinimal.Checked = true;
+                    break;
+                case NetworkLevel.Offline:
+                    radioNetworkUseOffline.Checked = true;
+                    break;
+            }
+            checkBoxHelpWithTesting.Checked = config.HelpWithTesting;
+            checkBoxAutoApproveKeys.Checked = config.AutoApproveKeys;
+            textBoxSyncServer.Uri = config.SyncServer;
+            textBoxSyncUsername.Text = config.SyncServerUsername;
+            textBoxSyncPassword.Text = config.SyncServerPassword;
+            textBoxSyncCryptoKey.Text = config.SyncCryptoKey;
 
-                // List all implementation directories in use
-                listBoxImplDirs.Items.Clear();
-                var userConfig = File.Exists(_implementationDirsConfigPath) ? File.ReadAllLines(_implementationDirsConfigPath, Encoding.UTF8) : new string[0];
-                foreach (string implementationDir in StoreFactory.GetImplementationDirs())
-                {
-                    // Differentiate between directories that can be modified (because they are listed in the user config) and those that cannot
-                    if (userConfig.Contains(implementationDir)) listBoxImplDirs.Items.Add(new DirectoryStore(implementationDir)); // DirectoryStore = can be modified
-                    else listBoxImplDirs.Items.Add(implementationDir); // Plain string = cannot be modified
-                }
+            // List all implementation directories in use
+            listBoxImplDirs.Items.Clear();
+            var userConfig = File.Exists(_implementationDirsConfigPath) ? File.ReadAllLines(_implementationDirsConfigPath, Encoding.UTF8) : new string[0];
+            foreach (string implementationDir in StoreFactory.GetImplementationDirs())
+            {
+                // Differentiate between directories that can be modified (because they are listed in the user config) and those that cannot
+                if (userConfig.Contains(implementationDir)) listBoxImplDirs.Items.Add(new DirectoryStore(implementationDir)); // DirectoryStore = can be modified
+                else listBoxImplDirs.Items.Add(implementationDir); // Plain string = cannot be modified
+            }
 
-                // List all catalog sources in use
-                listBoxCatalogSources.Items.Clear();
-                // ReSharper disable once CoVariantArrayConversion
-                listBoxCatalogSources.Items.AddRange(CatalogManager.GetCatalogSources());
+            // List all catalog sources in use
+            listBoxCatalogSources.Items.Clear();
+            // ReSharper disable once CoVariantArrayConversion
+            listBoxCatalogSources.Items.AddRange(CatalogManager.GetCatalogSources());
 
-                // Read list of trusted keys
-                var trustDB = TrustDB.LoadSafe();
-                var trustNodes = new NamedCollection<TrustNode>();
-                trustDB.Keys.Apply(key => key.Domains.Apply(domain => trustNodes.Add(new TrustNode(key.Fingerprint, domain))));
-                _treeViewTrustedKeys.Nodes = trustNodes;
-            }
-                #region Error handling
-            catch (IOException ex)
-            {
-                Msg.Inform(this, Resources.ProblemLoadingOptions + "\n" + ex.Message, MsgSeverity.Error);
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                Msg.Inform(this, Resources.ProblemLoadingOptions + "\n" + ex.Message, MsgSeverity.Error);
-            }
-            catch (InvalidDataException ex)
-            {
-                Msg.Inform(this, Resources.ProblemLoadingOptions + "\n" + ex.Message + (ex.InnerException == null ? "" : "\n" + ex.InnerException.Message), MsgSeverity.Error);
-            }
-            #endregion
+            // Read list of trusted keys
+            var trustDB = TrustDB.LoadSafe();
+            var trustNodes = new NamedCollection<TrustNode>();
+            trustDB.Keys.Apply(key => key.Domains.Apply(domain => trustNodes.Add(new TrustNode(key.Fingerprint, domain))));
+            _treeViewTrustedKeys.Nodes = trustNodes;
         }
 
         private void SaveConfig()
         {
-            try
-            {
-                // Write data from fields back to config
-                var config = Config.Load();
-                if (radioNetworkUseFull.Checked) config.NetworkUse = NetworkLevel.Full;
-                if (radioNetworkUseMinimal.Checked) config.NetworkUse = NetworkLevel.Minimal;
-                if (radioNetworkUseOffline.Checked) config.NetworkUse = NetworkLevel.Offline;
-                config.AutoApproveKeys = checkBoxAutoApproveKeys.Checked;
-                config.HelpWithTesting = checkBoxHelpWithTesting.Checked;
-                config.SyncServer = textBoxSyncServer.Uri;
-                config.SyncServerUsername = textBoxSyncUsername.Text;
-                config.SyncServerPassword = textBoxSyncPassword.Text;
-                config.SyncCryptoKey = textBoxSyncCryptoKey.Text;
-                config.Save();
+            // Write data from fields back to config
+            var config = Config.Load();
+            if (radioNetworkUseFull.Checked) config.NetworkUse = NetworkLevel.Full;
+            if (radioNetworkUseMinimal.Checked) config.NetworkUse = NetworkLevel.Minimal;
+            if (radioNetworkUseOffline.Checked) config.NetworkUse = NetworkLevel.Offline;
+            config.AutoApproveKeys = checkBoxAutoApproveKeys.Checked;
+            config.HelpWithTesting = checkBoxHelpWithTesting.Checked;
+            config.SyncServer = (textBoxSyncServer.IsValid && !string.IsNullOrEmpty(textBoxSyncServer.Text)) ? textBoxSyncServer.Uri : new Uri(Config.DefaultSyncServer);
+            config.SyncServerUsername = textBoxSyncUsername.Text;
+            config.SyncServerPassword = textBoxSyncPassword.Text;
+            config.SyncCryptoKey = textBoxSyncCryptoKey.Text;
+            config.Save();
 
-                // Write list of user implementation directories
-                WriteConfigFile(_implementationDirsConfigPath, listBoxImplDirs.Items.OfType<DirectoryStore>());
+            // Write list of user implementation directories
+            WriteConfigFile(_implementationDirsConfigPath, listBoxImplDirs.Items.OfType<DirectoryStore>());
 
-                // Write list of catalog sources
-                WriteConfigFile(_catalogSourcesConfigPath, listBoxCatalogSources.Items.OfType<string>());
+            // Write list of catalog sources
+            WriteConfigFile(_catalogSourcesConfigPath, listBoxCatalogSources.Items.OfType<string>());
 
-                // Write list of trusted keys
-                var trustDB = new TrustDB();
-                foreach (var trustNode in _treeViewTrustedKeys.Nodes)
-                    trustDB.TrustKey(trustNode.Fingerprint, trustNode.Domain);
-                trustDB.Save();
-            }
-                #region Error handling
-            catch (IOException ex)
-            {
-                Msg.Inform(this, Resources.ProblemSavingOptions + "\n" + ex.Message, MsgSeverity.Error);
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                Msg.Inform(this, Resources.ProblemSavingOptions + "\n" + ex.Message, MsgSeverity.Error);
-            }
-            catch (InvalidDataException ex)
-            {
-                Msg.Inform(this, Resources.ProblemSavingOptions + "\n" + ex.Message, MsgSeverity.Error);
-            }
-            #endregion
+            // Write list of trusted keys
+            var trustDB = new TrustDB();
+            foreach (var trustNode in _treeViewTrustedKeys.Nodes)
+                trustDB.TrustKey(trustNode.Fingerprint, trustNode.Domain);
+            trustDB.Save();
         }
         #endregion
 
@@ -328,7 +314,24 @@ namespace ZeroInstall.Central.WinForms
         #region Global buttons
         private void buttonApplyOK_Click(object sender, EventArgs e)
         {
-            SaveConfig();
+            try
+            {
+                SaveConfig();
+            }
+                #region Error handling
+            catch (IOException ex)
+            {
+                Msg.Inform(this, Resources.ProblemSavingOptions + "\n" + ex.Message, MsgSeverity.Error);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Msg.Inform(this, Resources.ProblemSavingOptions + "\n" + ex.Message, MsgSeverity.Error);
+            }
+            catch (InvalidDataException ex)
+            {
+                Msg.Inform(this, Resources.ProblemSavingOptions + "\n" + ex.Message, MsgSeverity.Error);
+            }
+            #endregion
         }
         #endregion
     }
