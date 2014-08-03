a8878e585c1272ee598417a8b25e2b51e00f8d3b
Bastian Eicher <bastian@eicher.net>
2014-08-03 13:58:00 +0200

    Revert "EnvironmentBinding: Use empty string as default for Value and Insert"
    This reverts commit fe6f93aa2c9d2552cb624d78143d55b67eb4ca74.

--- src/Backend/Store/Model/EnvironmentBinding.cs
+++ src/Backend/Store/Model/EnvironmentBinding.cs
@@ -63,7 +63,7 @@ namespace ZeroInstall.Store.Model
         /// </summary>
         /// <remarks>If this is set <see cref="Insert"/> must be <see langword="null"/>.</remarks>
         [Description("A static value to set the variable to. If this is set 'Insert' must be empty.")]
-        [XmlAttribute("value"), DefaultValue("")]
+        [XmlAttribute("value")]
         public string Value { get; set; }
 
         /// <summary>
@@ -71,7 +71,7 @@ namespace ZeroInstall.Store.Model
         /// </summary>
         /// <remarks>If this is set <see cref="Value"/> must be <see langword="null"/>.</remarks>
         [Description("The relative path of the item within the implementation to insert into the variable's value. Use \".\" to publish the root directory. If this is set 'Value' must be empty.")]
-        [XmlAttribute("insert"), DefaultValue("")]
+        [XmlAttribute("insert")]
         public string Insert { get; set; }
 
         /// <summary>
