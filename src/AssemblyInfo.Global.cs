4e154b2e4aa3fb18f4f8c5f5a8608098081b41d1
Bastian Eicher <bastian@eicher.net>
2014-09-05 17:48:03 +0200

    Incremented version number

--- src/AssemblyInfo.Global.cs
+++ src/AssemblyInfo.Global.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright 2010-2014 Bastian Eicher et al")]
 
 // Version information
-[assembly: AssemblyVersion("2.6.5")]
-[assembly: AssemblyFileVersion("2.6.5")]
+[assembly: AssemblyVersion("2.6.6")]
+[assembly: AssemblyFileVersion("2.6.6")]
