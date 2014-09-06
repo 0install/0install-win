8189e8c2aacb7501a1b2aec645f01646387eff21
Bastian Eicher <bastian@eicher.net>
2014-09-06 09:06:22 +0200

    Incremented version number

--- src/Updater/AssemblyInfo.Updater.cs
+++ src/Updater/AssemblyInfo.Updater.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright 2010-2014 Bastian Eicher")]
 
 // Version information
-[assembly: AssemblyVersion("1.3.2")]
-[assembly: AssemblyFileVersion("1.3.2")]
+[assembly: AssemblyVersion("1.3.3")]
+[assembly: AssemblyFileVersion("1.3.3")]
