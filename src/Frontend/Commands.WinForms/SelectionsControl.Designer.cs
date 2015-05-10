799475ab1de9f158f4d18e81a6a23ca466dd5f24
Bastian Eicher <bastian@eicher.net>
2015-05-10 19:18:25 +0200

    Cleaned up RESX files

--- src/Frontend/Commands.WinForms/SelectionsControl.Designer.cs
+++ src/Frontend/Commands.WinForms/SelectionsControl.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectionsControl));
             this.tableLayout = new System.Windows.Forms.TableLayoutPanel();
             this.labelName = new System.Windows.Forms.Label();
             this.labelVersion = new System.Windows.Forms.Label();
@@ -40,39 +39,68 @@
             // 
             // tableLayout
             // 
-            resources.ApplyResources(this.tableLayout, "tableLayout");
+            this.tableLayout.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayout.AutoSize = true;
+            this.tableLayout.ColumnCount = 3;
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.tableLayout.Controls.Add(this.labelName, 0, 0);
             this.tableLayout.Controls.Add(this.labelVersion, 1, 0);
             this.tableLayout.Controls.Add(this.labelTask, 2, 0);
+            this.tableLayout.Location = new System.Drawing.Point(0, 0);
             this.tableLayout.Name = "tableLayout";
+            this.tableLayout.RowCount = 1;
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayout.Size = new System.Drawing.Size(279, 19);
+            this.tableLayout.TabIndex = 0;
             // 
             // labelName
             // 
-            resources.ApplyResources(this.labelName, "labelName");
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(3, 0);
             this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
             // 
             // labelVersion
             // 
-            resources.ApplyResources(this.labelVersion, "labelVersion");
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Location = new System.Drawing.Point(68, 0);
             this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(42, 13);
+            this.labelVersion.TabIndex = 1;
+            this.labelVersion.Text = "Version";
             // 
             // labelTask
             // 
-            resources.ApplyResources(this.labelTask, "labelTask");
+            this.labelTask.AutoSize = true;
+            this.labelTask.Location = new System.Drawing.Point(128, 0);
             this.labelTask.Name = "labelTask";
+            this.labelTask.Size = new System.Drawing.Size(31, 13);
+            this.labelTask.TabIndex = 2;
+            this.labelTask.Text = "Task";
             // 
             // scrollPanel
             // 
-            resources.ApplyResources(this.scrollPanel, "scrollPanel");
+            this.scrollPanel.AutoScroll = true;
             this.scrollPanel.Controls.Add(this.tableLayout);
+            this.scrollPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scrollPanel.Location = new System.Drawing.Point(0, 0);
+            this.scrollPanel.Margin = new System.Windows.Forms.Padding(0);
             this.scrollPanel.Name = "scrollPanel";
+            this.scrollPanel.Size = new System.Drawing.Size(279, 61);
+            this.scrollPanel.TabIndex = 0;
             // 
             // SelectionsControl
             // 
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.scrollPanel);
             this.Name = "SelectionsControl";
+            this.Size = new System.Drawing.Size(279, 61);
             this.tableLayout.ResumeLayout(false);
             this.tableLayout.PerformLayout();
             this.scrollPanel.ResumeLayout(false);
