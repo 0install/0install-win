003ba9ceb8f47bfe20fb27a5053c74a06fef9ddc
Bastian Eicher <bastian@eicher.net>
2016-02-15 15:20:57 +0100

    Use local build of 0install to run Doxygen
    This allows a run of "build.cmd" to succeed on a machine that does not have a release of Zero Install deployed on it yet.

--- doc/build.cmd
+++ doc/build.cmd
@@ -8,11 +8,11 @@ if exist ..\..\..\nano-byte\common\build\Documentation\common.tag copy ..\..\..\
 echo Building source documentation...
 if exist ..\build\Documentation rd /s /q ..\build\Documentation
 mkdir ..\build\Documentation
-0install run http://0install.de/feeds/Doxygen.xml Backend.Doxyfile
+..\build\Release\Frontend\0install.exe run http://0install.de/feeds/Doxygen.xml Backend.Doxyfile
 if errorlevel 1 exit /b %errorlevel%
-0install run http://0install.de/feeds/Doxygen.xml Frontend.Doxyfile
+..\build\Release\Frontend\0install.exe run http://0install.de/feeds/Doxygen.xml Frontend.Doxyfile
 if errorlevel 1 exit /b %errorlevel%
-0install run http://0install.de/feeds/Doxygen.xml Tools.Doxyfile
+..\build\Release\Frontend\0install.exe run http://0install.de/feeds/Doxygen.xml Tools.Doxyfile
 if errorlevel 1 exit /b %errorlevel%
 
 popd
