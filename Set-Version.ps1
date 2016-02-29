dfdfa25374ff664328a77af63269794cfd169021
Bastian Eicher <bastian@eicher.net>
2016-02-29 22:07:06 +0100

    Tweaked Set-Version script, renamed AssemblyInfo.Global.cs to GlobalAssemblyInfo.cs

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -1,15 +1,13 @@
-﻿#Sets a new version number in all relevant locations
+﻿Param ([Parameter(Mandatory=$True)] [string]$NewVersion)
+#Sets a new version number in all relevant locations
 $ErrorActionPreference = "Stop"
 $ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
-if ($args.Length -lt 1) { Write-Error "Missing argument" }
-$NewVersion = $args[0]
-
 [System.IO.File]::WriteAllText("$ScriptDir\VERSION", $NewVersion)
 
-(Get-Content "$ScriptDir\src\AssemblyInfo.Global.cs" -Encoding UTF8) `
+(Get-Content "$ScriptDir\src\GlobalAssemblyInfo.cs" -Encoding UTF8) `
   -replace 'AssemblyVersion\(".*"\)', ('AssemblyVersion("' + $NewVersion + '")') |
-  Set-Content "$ScriptDir\src\AssemblyInfo.Global.cs" -Encoding UTF8
+  Set-Content "$ScriptDir\src\GlobalAssemblyInfo.cs" -Encoding UTF8
 (Get-Content "$ScriptDir\src\Frontend\OneGet\provider.manifest" -Encoding UTF8) `
   -replace 'version=".*" versionScheme="multipartnumeric"', ('version="' + $NewVersion + '.0" versionScheme="multipartnumeric"') |
   Set-Content "$ScriptDir\src\Frontend\OneGet\provider.manifest" -Encoding UTF8
