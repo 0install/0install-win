c5fe8bf112b74d332cf772f566f767431503bed1
Bastian Eicher <bastian@eicher.net>
2016-08-28 23:39:46 +0200

    Added OneGet support to Bootstrap

---
+++ src/Bootstrap/OneGet/BootstrapOneGetContext.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using NanoByte.Common;
+using NanoByte.Common.Native;
+using NanoByte.Common.Storage;
+using PackageManagement.Sdk;
+using ZeroInstall.Store;
+using ZeroInstall.Store.Model;
+
+namespace ZeroInstall.OneGet
+{
+    /// <summary>
+    /// Pretends to handle OneGet <see cref="Request"/>s but instead bootstraps Zero Install (which contains the real package provider).
+    /// </summary>
+    public class BootstrapOneGetContext : IOneGetContext
+    {
+        private readonly Request _request;
+        private readonly OneGetHandler _handler;
+
+        public BootstrapOneGetContext(Request request)
+        {
+            _request = request;
+            _handler = new OneGetHandler(request);
+        }
+
+        public void Dispose()
+        {
+            _handler.Dispose();
+        }
+
+        public void GetDynamicOptions(string category)
+        {
+            _request.YieldDynamicOption("Scope", Constants.OptionType.String, isRequired: false, permittedValues: new[] {"CurrentUser", "AllUsers"});
+        }
+
+        private bool? MachineWide
+        {
+            get
+            {
+                switch (_request.GetOptionValue("Scope"))
+                {
+                    case "CurrentUser":
+                        return false;
+                    case "AllUsers":
+                        return true;
+                    default:
+                        return null;
+                }
+            }
+        }
+
+        public void AddPackageSource(FeedUri uri)
+        {
+            Deploy();
+        }
+
+        public void RemovePackageSource(FeedUri uri)
+        {
+            Deploy();
+        }
+
+        public void ResolvePackageSources()
+        {}
+
+        public void FindPackage(string name, ImplementationVersion requiredVersion, ImplementationVersion minimumVersion, ImplementationVersion maximumVersion)
+        {
+            Deploy();
+        }
+
+        public void FindPackageBy(string identifier)
+        {
+            Deploy();
+        }
+
+        private static bool _deployCompleted;
+
+        /// <summary>
+        /// Deploys Zero Install to the computer.
+        /// </summary>
+        private void Deploy()
+        {
+            const string openNewSessionMessage = "Please open a new PowerShell session to start using the '0install' package provider.";
+            if (_deployCompleted)
+            {
+                _request.Warning(openNewSessionMessage);
+                return;
+            }
+
+            var process = new BootstrapProcess(_handler, gui: false);
+            var result = process.Execute(MachineWide ?? WindowsUtils.IsAdministrator
+                ? new[] {"maintenance", "deploy", "--batch", "--machine"}
+                : new[] {"maintenance", "deploy", "--batch"});
+
+            if (result == ExitCode.OK)
+            {
+                _deployCompleted = true;
+                _request.Warning(openNewSessionMessage);
+
+                // Try to move Bootstrap provider after deployment to prevent future PowerShell sessions from loading it again
+                try
+                {
+                    File.Move(Program.ExePath, FileUtils.GetTempFile("0install"));
+                }
+                    #region Error handling
+                catch (IOException ex)
+                {
+                    Log.Debug(ex);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    Log.Debug(ex);
+                }
+                #endregion
+            }
+            else Log.Warn("Installed failed with exit code " + result + ".");
+        }
+
+        public void GetInstalledPackages(string name)
+        {}
+
+        public void DownloadPackage(string fastPackageReference, string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InstallPackage(string fastPackageReference)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UninstallPackage(string fastPackageReference)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetPackageDetails(string fastPackageReference)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Bootstrap/OneGet/BootstrapPackageProvider.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Copyright 2010-2016 Bastian Eicher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.IO;
+using JetBrains.Annotations;
+using NanoByte.Common;
+using PackageManagement.Sdk;
+
+namespace ZeroInstall.OneGet
+{
+    /// <summary>
+    /// A OneGet package provider that bootstraps/deploys Zero Install.
+    /// </summary>
+    [PublicAPI]
+    public class BootstrapPackageProvider : PackageProviderBase
+    {
+        /// <inheritdoc/>
+        protected override string Name => "0install";
+
+        /// <inheritdoc/>
+        protected override bool IsDisabled
+        {
+            get
+            {
+                // Disable the bootstrap provider when a real Zero Install instance is detected
+                string existingInstall = RegistryUtils.GetSoftwareString("Zero Install", "InstallLocation");
+                return !string.IsNullOrEmpty(existingInstall) && File.Exists(Path.Combine(existingInstall, "ZeroInstall.OneGet.dll"));
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override IOneGetContext BuildContext(Request request) => new BootstrapOneGetContext(request);
+    }
+}
