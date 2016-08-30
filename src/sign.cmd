5da77b9a782d1ca3f6a47432b2ccb39d0d75207e
Bastian Eicher <bastian@eicher.net>
2016-08-30 20:07:59 +0200

    Use sha256 for AuthentiCode

--- src/sign.cmd
+++ src/sign.cmd
@@ -1,7 +1,7 @@
 @echo off
 ::Adds AuthentiCode signatures to all binaries. Assumes "build.cmd Release" has already been executed.
 if not "%1" == "" set signing_cert_path=%*
-set timestamp_server=http://timestamp.comodoca.com/authenticode
+set timestamp_server=http://timestamp.globalsign.com/?signature=sha2
 
 rem Determine VS version
 if defined VS140COMNTOOLS (
@@ -17,23 +17,23 @@ exit /b 1
 
 echo Signing binaries with "%signing_cert_path%"...
 
-FOR %%A IN ("%~dp0..\build\Release\Frontend\*.exe") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
+FOR %%A IN ("%~dp0..\build\Release\Frontend\*.exe") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
 if errorlevel 1 exit /b %errorlevel%
-FOR %%A IN ("%~dp0..\build\Release\Frontend\ZeroInstall.*.dll") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
+FOR %%A IN ("%~dp0..\build\Release\Frontend\ZeroInstall.*.dll") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
 if errorlevel 1 exit /b %errorlevel%
-FOR %%A IN ("%~dp0..\build\Release\Tools\*.exe") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
+FOR %%A IN ("%~dp0..\build\Release\Tools\*.exe") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
 if errorlevel 1 exit /b %errorlevel%
-FOR %%A IN ("%~dp0..\build\Release\Tools\ZeroInstall.*.dll") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
-if errorlevel 1 exit /b %errorlevel%
-
-FOR %%A IN ("%~dp0..\build\ReleaseNet35\Frontend\*.exe") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
-if errorlevel 1 exit /b %errorlevel%
-FOR %%A IN ("%~dp0..\build\ReleaseNet35\Frontend\ZeroInstall.*.dll") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
+FOR %%A IN ("%~dp0..\build\Release\Tools\ZeroInstall.*.dll") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
 if errorlevel 1 exit /b %errorlevel%
 
-FOR %%A IN ("%~dp0..\build\ReleaseNet20\Frontend\*.exe") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
+FOR %%A IN ("%~dp0..\build\ReleaseNet35\Frontend\*.exe") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
 if errorlevel 1 exit /b %errorlevel%
-FOR %%A IN ("%~dp0..\build\ReleaseNet20\Frontend\ZeroInstall.*.dll") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
+FOR %%A IN ("%~dp0..\build\ReleaseNet35\Frontend\ZeroInstall.*.dll") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
 if errorlevel 1 exit /b %errorlevel%
-signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%~dp0..\build\ReleaseNet20\Bootstrap\zero-install.exe"
+
+FOR %%A IN ("%~dp0..\build\ReleaseNet20\Frontend\*.exe") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
+if errorlevel 1 exit /b %errorlevel%
+FOR %%A IN ("%~dp0..\build\ReleaseNet20\Frontend\ZeroInstall.*.dll") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
+if errorlevel 1 exit /b %errorlevel%
+signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%~dp0..\build\ReleaseNet20\Bootstrap\zero-install.exe"
 if errorlevel 1 exit /b %errorlevel%
