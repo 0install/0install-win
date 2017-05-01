132cd2f3ddea493161ba4b950eb1965b418556e3
Bastian Eicher <bastian@eicher.net>
2017-05-01 21:32:38 +0200

    Introduced CloneFile

---
+++ src/Backend/Store/Implementations/Build/CloneFile.cs
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2010-2017 Bastian Eicher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.IO;
+using JetBrains.Annotations;
+
+namespace ZeroInstall.Store.Implementations.Build
+{
+    /// <summary>
+    /// Copies the content of a single file to a new location preserving the original file modification time, executable bit and/or symlink status.
+    /// </summary>
+    public class CloneFile : CloneDirectory
+    {
+        /// <summary>
+        /// The name of the original file to read without any directory information.
+        /// </summary>
+        [NotNull]
+        public string SourceFileName { get; }
+
+        /// <summary>
+        /// The name of the new file to write without any directory information.
+        /// </summary>
+        [NotNull]
+        public string TargetFileName { get; set; }
+
+        /// <summary>
+        /// Creates a new file cloning task.
+        /// </summary>
+        /// <param name="sourceFilePath">The path of the original file to read.</param>
+        /// <param name="targetDirPath">The path of the new directory to clone the file to.</param>
+        public CloneFile([NotNull] string sourceFilePath, [NotNull] string targetDirPath)
+            : base(Path.GetDirectoryName(sourceFilePath), targetDirPath)
+        {
+            SourceFileName = Path.GetFileName(sourceFilePath);
+            TargetFileName = SourceFileName;
+        }
+
+        /// <inheritdoc/>
+        protected override void HandleFile(FileInfo file, bool executable = false)
+        {
+            #region Sanity checks
+            if (file == null) throw new ArgumentNullException(nameof(file));
+            #endregion
+
+            if (file.Name == SourceFileName)
+                base.HandleFile(file, executable);
+        }
+
+        /// <inheritdoc/>
+        protected override void HandleSymlink(FileSystemInfo symlink, string target)
+        {
+            #region Sanity checks
+            if (symlink == null) throw new ArgumentNullException(nameof(symlink));
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            #endregion
+
+            if (symlink.Name == SourceFileName)
+                DirectoryBuilder.CreateSymlink(TargetFileName, target);
+        }
+
+        /// <inheritdoc/>
+        protected override string NewFilePath(FileInfo file, bool executable)
+        {
+            #region Sanity checks
+            if (file == null) throw new ArgumentNullException(nameof(file));
+            #endregion
+
+            return DirectoryBuilder.NewFilePath(TargetFileName, file.LastWriteTimeUtc, executable);
+        }
+    }
+}
---
+++ src/Backend/UnitTests/Store/Implementations/Build/CloneFileTest.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Copyright 2010-2017 Bastian Eicher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.IO;
+using NanoByte.Common.Storage;
+using NUnit.Framework;
+using ZeroInstall.FileSystem;
+
+namespace ZeroInstall.Store.Implementations.Build
+{
+    /// <summary>
+    /// Contains test methods for <see cref="CloneFile"/>.
+    /// </summary>
+    [TestFixture]
+    public class CloneFileTest : CloneTestBase
+    {
+        [Test]
+        public void CopyFile()
+        {
+            new TestRoot
+            {
+                new TestFile("fileA"),
+                new TestFile("decoy") {Contents = "wrong"}
+            }.Build(SourceDirectory);
+
+            new CloneFile(Path.Combine(SourceDirectory, "fileA"), TargetDirectory) {TargetFileName = "fileB"}.Run();
+
+            new TestRoot {new TestFile("fileB")}.Verify(TargetDirectory);
+        }
+
+        [Test]
+        public void CopyFileSuffix()
+        {
+            new TestRoot {new TestFile("fileA")}.Build(SourceDirectory);
+
+            new CloneFile(Path.Combine(SourceDirectory, "fileA"), TargetDirectory) {TargetSuffix = "suffix", TargetFileName = "fileB"}.Run();
+
+            new TestRoot {new TestDirectory("suffix") {new TestFile("fileB")}}.Verify(TargetDirectory);
+        }
+
+        [Test]
+        public void HardlinkFile()
+        {
+            new TestRoot {new TestFile("fileA")}.Build(SourceDirectory);
+
+            FileUtils.EnableWriteProtection(SourceDirectory); // Hardlinking logic should work around write-protection by temporarily removing it
+            try
+            {
+                new CloneFile(Path.Combine(SourceDirectory, "fileA"), TargetDirectory) {TargetFileName = "fileB", UseHardlinks = true}.Run();
+            }
+            finally
+            {
+                FileUtils.DisableWriteProtection(SourceDirectory);
+            }
+
+            new TestRoot {new TestFile("fileB")}.Verify(TargetDirectory);
+            FileUtils.AreHardlinked(Path.Combine(SourceDirectory, "fileA"), Path.Combine(TargetDirectory, "fileB"));
+        }
+
+        [Test]
+        public void CopySymlink()
+        {
+            new TestRoot {new TestSymlink("fileA", "target")}.Build(SourceDirectory);
+
+            new CloneFile(Path.Combine(SourceDirectory, "fileA"), TargetDirectory) {TargetFileName = "fileB"}.Run();
+
+            new TestRoot {new TestSymlink("fileB", "target")}.Verify(TargetDirectory);
+        }
+
+        [Test]
+        public void OverwriteFile()
+        {
+            new TestRoot {new TestFile("fileA")}.Build(SourceDirectory);
+            new TestRoot {new TestFile("fileB") {LastWrite = new DateTime(2000, 2, 2), Contents = "wrong", IsExecutable = true}}.Build(TargetDirectory);
+
+            new CloneFile(Path.Combine(SourceDirectory, "fileA"), TargetDirectory) {TargetFileName = "fileB"}.Run();
+
+            new TestRoot {new TestFile("fileB")}.Verify(TargetDirectory);
+        }
+
+        [Test]
+        public void OverwriteSymlink()
+        {
+            new TestRoot {new TestFile("fileA")}.Build(SourceDirectory);
+            new TestRoot {new TestSymlink("fileB", "target")}.Build(TargetDirectory);
+
+            new CloneFile(Path.Combine(SourceDirectory, "fileA"), TargetDirectory) {TargetFileName = "fileB"}.Run();
+
+            new TestRoot {new TestFile("fileB")}.Verify(TargetDirectory);
+        }
+
+        [Test]
+        public void OverwriteWithSymlink()
+        {
+            new TestRoot {new TestSymlink("fileA", "target")}.Build(SourceDirectory);
+            new TestRoot {new TestFile("fileB")}.Build(TargetDirectory);
+
+            new CloneFile(Path.Combine(SourceDirectory, "fileA"), TargetDirectory) {TargetFileName = "fileB"}.Run();
+
+            new TestRoot {new TestSymlink("fileB", "target")}.Verify(TargetDirectory);
+        }
+    }
+}
