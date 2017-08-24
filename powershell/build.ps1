c762abb37729bcb8877dd450c046f092e0d6675c
Bastian Eicher <bastian@eicher.net>
2017-08-24 15:00:23 +0200

    Fixed PowerShell Module build

---
+++ powershell/build.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = "Stop"
+pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+if (!(Test-Path ..\build\PowerShellModules\0install)) { mkdir ..\build\PowerShellModules\0install | Out-Null }
+(Get-Content 0install.psd1.template -Encoding UTF8) `
+  -replace '{version}', $(Get-Content ..\VERSION) |
+  Set-Content ..\build\PowerShellModules\0install\0install.psd1 -Encoding UTF8
+
+copy ..\build\Bootstrap\zero-install.exe ..\build\PowerShellModules\0install\0install.dll
+
+popd
