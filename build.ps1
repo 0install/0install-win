c580926d843e9af1ac12e9267aaef2d6714b538b
Bastian Eicher <bastian@eicher.net>
2017-09-05 18:00:37 +0200

    Fixed support for "./build.ps1 -Deploy"

--- build.ps1
+++ build.ps1
@@ -5,15 +5,15 @@ $RootDir = $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 pushd $RootDir
 
 src\build.ps1
+powershell\build.ps1
 doc\build.ps1
 release\build.ps1
-powershell\build.ps1
 
 if ($Deploy) {
   if ($Machine) {
-    build\Bootstrap\zero-install.exe --feed="$RootDir\release\ZeroInstall-$(Get-Content ..\VERSION).xml" maintenance --batch --machine
+    build\Bootstrap\zero-install.exe --feed="$RootDir\release\ZeroInstall-$(Get-Content VERSION).xml" maintenance deploy --batch --machine
   } else {
-    build\Bootstrap\zero-install.exe --feed="$RootDir\release\ZeroInstall-$(Get-Content ..\VERSION).xml" maintenance --batch
+    build\Bootstrap\zero-install.exe --feed="$RootDir\release\ZeroInstall-$(Get-Content VERSION).xml" maintenance deploy --batch
   }
 }
 
