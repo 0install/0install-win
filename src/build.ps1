38e410e2d89418578374229d72a18878330fc400
Bastian Eicher <bastian@eicher.net>
2017-11-13 08:42:16 +0100

    Tweaked build script

--- src/build.ps1
+++ src/build.ps1
@@ -3,6 +3,6 @@ pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 nuget restore
 . "$(. "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value)\Common7\IDE\devenv.com" ZeroInstall.sln /Build Release
-nuget pack ZeroInstall.Frontend.nuspec -Properties "Configuration=Release;Version=$(Get-Content ..\VERSION)" -Symbols -OutputDirectory ..\build
+nuget pack ZeroInstall.Frontend.nuspec -Properties Version=$(Get-Content ..\VERSION) -Symbols -OutputDirectory ..\build
 
 popd
