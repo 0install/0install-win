1626421a5b59c7b3c2b2ecae0b3727305f78ae0a
Bastian Eicher <bastian@eicher.net>
2018-01-07 15:39:09 +0100

    Tweaked copyright notices

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 // Assembly info
 [assembly: AssemblyCompany("0install.de")]
 [assembly: AssemblyProduct("Zero Install")]
-[assembly: AssemblyCopyright("Copyright 2010-2017 Bastian Eicher et al")]
+[assembly: AssemblyCopyright("Copyright Bastian Eicher et al.")]
 
 // Version information
 [assembly: AssemblyVersion("2.14.5")]
