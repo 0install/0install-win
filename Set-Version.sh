d3fe5a14d90ea08e7e885bd7a61d13ba1d486431
Bastian Eicher <bastian@eicher.net>
2018-03-28 23:52:14 +0200

    Minor tweaks

--- Set-Version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd `dirname $0`
-
-printf $1 > VERSION
-sed -b -i "s/PROJECT_NUMBER = \".*\"/PROJECT_NUMBER = \"$1\"/" doc/Doxyfile
-sed -b -i "s/AssemblyVersion\(\".*\"\)/AssemblyVersion\(\"$1\"\)/" src/GlobalAssemblyInfo.cs
-sed -b -i "s/version=\".*\" versionScheme=\"multipartnumeric\"/version=\"$1\" versionScheme=\"multipartnumeric\"/" src/OneGet/provider.manifest
