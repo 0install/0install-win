8cc7cb7873b71ca3bbab5a24de859902ffd55fe1
Bastian Eicher <bastian@eicher.net>
2018-05-13 21:09:32 +0200

    Added high-resolution logos for Start Menu on Windows 8.1+

--- release/build.ps1
+++ release/build.ps1
@@ -4,7 +4,10 @@ pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 # Ensure 0install is in the PATH
 if (!(Get-Command 0install -ErrorAction SilentlyContinue)) { $env:PATH = "$(Resolve-Path ..\build\Release);$env:PATH" }
 
-rm -Force ..\build\Release\*.xml
+# Do not include .NET XML documentation in release
+rm -Force ..\build\Release\*.xml -Exclude *.VisualElementsManifest.xml
+
+# Do not include debug symbols in release
 rm -Force ..\build\Release\*.pdb
 
 cmd /c "0install run --batch http://0install.net/tools/0template.xml ZeroInstall.xml.template version=$(Get-Content ..\VERSION) 2>&1" # Redirect stderr to stdout
