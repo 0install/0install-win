76d137c7552450d7a6b03019fb606d8bbd40bf96
Bastian Eicher <bastian@eicher.net>
2018-06-08 15:27:06 +0200

    Patch URL in feed to point to GitHub release for tagged builds

--- release/build.ps1
+++ release/build.ps1
@@ -1,6 +1,8 @@
 ﻿$ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
+$Version = Get-Content ..\VERSION
+
 # Ensure 0install is in the PATH
 if (!(Get-Command 0install -ErrorAction SilentlyContinue)) { $env:PATH = "$(Resolve-Path ..\build\Release);$env:PATH" }
 
@@ -10,7 +12,16 @@ rm -Force ..\build\Release\*.xml -Exclude *.VisualElementsManifest.xml
 # Do not include debug symbols in release
 rm -Force ..\build\Release\*.pdb
 
-cmd /c "0install run --batch http://0install.net/tools/0template.xml ZeroInstall.xml.template version=$(Get-Content ..\VERSION) 2>&1" # Redirect stderr to stdout
+# Build feed and archive
+cmd /c "0install run --batch http://0install.net/tools/0template.xml ZeroInstall.xml.template version=$Version 2>&1" # Redirect stderr to stdout
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+
+# Patch URL to point to GitHub release for tagged builds
+if ($env:APPVEYOR_REPO_TAG -eq "true") {
+    $path = Resolve-Path "ZeroInstall-$Version.xml"
+    [xml]$xml = Get-Content $path
+    $xml.interface.group.implementation.archive.href = "https://github.com/0install/0install-win/releases/download/$Version/$($xml.interface.group.implementation.archive.href)"
+    $xml.Save($path)
+}
 
 popd
