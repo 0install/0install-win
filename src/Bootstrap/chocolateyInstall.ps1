d4973c8da25d591ad29e7e193c0c96771e2c31f1
Bastian Eicher <bastian@eicher.net>
2018-12-11 23:47:53 +0100

    Tweaked Chocolatey install script

--- src/Bootstrap/chocolateyInstall.ps1
+++ src/Bootstrap/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-﻿cd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+﻿Set-Location $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 .\0install.exe --version=0.1.0-pre maintenance deploy --machine --batch
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
