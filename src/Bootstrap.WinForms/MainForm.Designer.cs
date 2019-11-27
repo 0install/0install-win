1cfb48ad94afb139890415d83abe5203c4a83cdc
Bastian Eicher <bastian@eicher.net>
2019-11-27 02:07:11 +0100

    Bootstrap GUI: Made status label larger

--- src/Bootstrap.WinForms/MainForm.Designer.cs
+++ src/Bootstrap.WinForms/MainForm.Designer.cs
@@ -64,13 +64,14 @@ namespace ZeroInstall
             // 
             this.labelLoading.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelLoading.AutoEllipsis = true;
             this.labelLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelLoading.Location = new System.Drawing.Point(12, 97);
             this.labelLoading.Name = "labelLoading";
-            this.labelLoading.Size = new System.Drawing.Size(374, 43);
+            this.labelLoading.Size = new System.Drawing.Size(374, 89);
             this.labelLoading.TabIndex = 0;
             this.labelLoading.Text = "Loading...";
-            this.labelLoading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelLoading.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // buttonClose
             // 
