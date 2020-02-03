b031915b9bcfae4ec0a9371526152ffc6e6c7c8a
Bastian Eicher <bastian@eicher.net>
2020-02-03 18:46:53 +0100

    Support VS Build Tools in build scripts

--- src/build.ps1
+++ src/build.ps1
@@ -18,7 +18,7 @@ SearchAndReplace $AssemblyVersion OneGet\provider.manifest -PatternLeft 'version
 SearchAndReplace $AssemblyVersion OneGet.Bootstrap\0install.psd1 -PatternLeft "ModuleVersion = '" -PatternRight "'"
 
 # Compile source code
-$vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value
+$vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -latest -property installationPath -format value
 $msBuild = if (Test-Path "$vsDir\MSBuild\Current") {"$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe"} else {"$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"}
 . $msBuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
