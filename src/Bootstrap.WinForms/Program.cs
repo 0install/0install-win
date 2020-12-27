027ace1a27cf7e490dbc319b4e01b158d398c98d
Bastian Eicher <bastian@eicher.net>
2020-12-27 17:52:05 +0100

    Removed CLI-mode from GUI bootstrapper

--- src/Bootstrap.WinForms/Program.cs
+++ src/Bootstrap.WinForms/Program.cs
@@ -1,78 +1,15 @@
 // Copyright Bastian Eicher et al.
 // Licensed under the GNU Lesser Public License
 
-using System;
 using System.Windows.Forms;
 using NanoByte.Common.Controls;
-using NanoByte.Common.Native;
-using NanoByte.Common.Tasks;
+using ZeroInstall;
 
-namespace ZeroInstall
-{
-    public static class Program
-    {
-        public static int Main(string[] args)
-        {
-            ProgramUtils.Init();
+ProgramUtils.Init();
+Application.EnableVisualStyles();
+Application.SetCompatibleTextRenderingDefault(false);
 
-            if (WindowsUtils.IsWindows)
-            {
-                if (WindowsUtils.AttachConsole())
-                    return (int)RunCliRedirect(args);
-                else
-                    return (int)RunGui(args);
-            }
-            else return (int)RunCli(args);
-        }
+ErrorReportForm.SetupMonitoring(new("https://0install.de/error-report/"));
 
-        /// <summary>
-        /// Runs the application in command-line mode.
-        /// </summary>
-        /// <param name="args">The command-line arguments passed to the application.</param>
-        /// <returns>The exit status code to end the process with.</returns>
-        public static ExitCode RunCli(string[] args)
-        {
-            using var handler = new CliTaskHandler();
-            return ProgramUtils.Run(args, handler, gui: false);
-        }
-
-        /// <summary>
-        /// Runs the application in command-line mode with special-case handling for retroactively attached Windows consoles.
-        /// </summary>
-        /// <param name="args">The command-line arguments passed to the application.</param>
-        /// <returns>The exit status code to end the process with.</returns>
-        public static ExitCode RunCliRedirect(string[] args)
-        {
-            using var handler = new CliTaskHandler();
-            try
-            {
-                Console.WriteLine();
-                return ProgramUtils.Run(args, handler, gui: false);
-            }
-            finally
-            {
-                if (handler.Verbosity != Verbosity.Batch)
-                {
-                    Console.WriteLine();
-                    Console.Write(Environment.CurrentDirectory + @">");
-                }
-            }
-        }
-
-        /// <summary>
-        /// Runs the application in GUI mode.
-        /// </summary>
-        /// <param name="args">The command-line arguments passed to the application.</param>
-        /// <returns>The exit status code to end the process with.</returns>
-        public static ExitCode RunGui(string[] args)
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            ErrorReportForm.SetupMonitoring(new("https://0install.de/error-report/"));
-
-            using var handler = new GuiTaskHandler();
-            return ProgramUtils.Run(args, handler, gui: true);
-        }
-    }
-}
+using var handler = new GuiTaskHandler();
+return (int)ProgramUtils.Run(args, handler, gui: true);
