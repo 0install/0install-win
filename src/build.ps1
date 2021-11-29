3a75772c0bf477e0e7ad448b7357c5c4afc78094
Bastian Eicher <bastian@eicher.net>
2021-11-29 21:42:32 +0100

    Tweaked build script

--- src/build.ps1
+++ src/build.ps1
@@ -12,11 +12,7 @@ function Find-MSBuild {
 }
 
 function Run-DotNet {
-    if (Get-Command dotnet -ErrorAction SilentlyContinue) {
-        dotnet @args
-    } else {
-        ..\0install.ps1 run --batch --version 5.0..!5.1 https://apps.0install.net/dotnet/core-sdk.xml @args
-    }
+    ..\0install.ps1 run --batch --version 5.0.. https://apps.0install.net/dotnet/sdk.xml @args
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
