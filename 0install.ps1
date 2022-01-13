14631d4b9e120c9e4bd0d48bc9f8c98cd90fcc04
Bastian Eicher <bastian@eicher.net>
2022-01-13 18:14:16 +0100

    Updated 0install bootstrap scripts

--- 0install.ps1
+++ 0install.ps1
@@ -1,18 +1,22 @@
 $ErrorActionPreference = "Stop"
 
-function download {
-    $downloadDir = "$env:LOCALAPPDATA\0install.net\bootstrapper"
-    if (!(Test-Path "$downloadDir\0install.exe")) {
-        mkdir -Force $downloadDir | Out-Null
+function Download-ZeroInstall {
+    $dir = "$env:LOCALAPPDATA\0install.net\bootstrapper"
+    $file = "$dir\0install.exe"
+    if (!(Test-Path $file)) {
+        mkdir -Force $dir | Out-Null
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls11,Tls12'
-        Invoke-WebRequest "https://get.0install.net/0install.exe" -OutFile "$downloadDir\0install.exe"
+        Invoke-WebRequest "https://get.0install.net/0install.exe" -OutFile $file
     }
-    return $downloadDir
+    return $file
 }
 
-$ErrorActionPreference = "SilentlyContinue"
-if (Get-Command 0install) {
-    0install @args | %{ "$_" }
-} else {
-    . "$(download)\0install.exe" @args | %{ "$_" }
+function Run-ZeroInstall {
+    if (Get-Command 0install -ErrorAction SilentlyContinue) {
+        0install @args | %{ "$_" }
+    } else {
+        . $(Download-ZeroInstall) @args | %{ "$_" }
+    }
 }
+
+Run-ZeroInstall @args
