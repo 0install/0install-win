e5fc8c28b7f85331a9e804d08b8b76af339618e9
Bastian Eicher <bastian@eicher.net>
2022-03-28 18:31:25 +0200

    Switched to Visual Studio 2022 and C# 10

--- src/build.ps1
+++ src/build.ps1
@@ -4,16 +4,11 @@ pushd $PSScriptRoot
 
 function Find-MSBuild {
     if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
-        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 16.8
+        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 17.0
         if ($vsDir) {
             if (Test-Path "$vsDir\MSBuild\Current") { return "$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe" } else { return "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe" }
         }
     }
-}
-
-function Run-DotNet {
-    ..\0install.ps1 run --batch --version 5.0.. https://apps.0install.net/dotnet/sdk.xml @args
-    if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
 function Run-MSBuild {
@@ -22,8 +17,7 @@ function Run-MSBuild {
         . $msbuild @args
         if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
     } else {
-        Write-Warning "You need Visual Studio 2019 v16.8+ to perform a full build of this project"
-        Run-DotNet msbuild @args
+        throw "You need Visual Studio 2022 or newer build this project"
     }
 }
 
