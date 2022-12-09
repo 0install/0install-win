5349c44782751bec37e125e591fffefb726ca979
Bastian Eicher <bastian@eicher.net>
2022-12-09 21:57:00 +0100

    Use .NET SDK 7.0 and C# 11

--- src/build.ps1
+++ src/build.ps1
@@ -4,7 +4,7 @@ pushd $PSScriptRoot
 
 function Find-MSBuild {
     if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
-        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 17.0
+        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 17.4
         if ($vsDir) {
             if (Test-Path "$vsDir\MSBuild\Current") { return "$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe" } else { return "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe" }
         }
@@ -13,7 +13,7 @@ function Find-MSBuild {
 
 function Run-MSBuild {
     $msbuild = Find-Msbuild
-    if (!$msbuild) { throw "You need Visual Studio 2022 or newer to build this project" }
+    if (!$msbuild) { throw "You need Visual Studio 2022 v17.4 or newer to build this project" }
     . $msbuild @args
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
