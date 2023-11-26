996863678b1e0ba497ff1888c0d900f45d5c67d9
Bastian Eicher <bastian@eicher.net>
2023-11-26 02:55:41 +0100

    Cover more archive formats in integration tests

--- tests/download.ps1
+++ tests/download.ps1
@@ -1,2 +1,2 @@
-0install-win download --batch https://apps.0install.net/devel/terraform.xml | Out-Null
+0install-win download --batch https://apps.0install.net/gui/naps2.xml | Out-Null
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
