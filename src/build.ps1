65fdb1e9230c2afd834831757f91a999fb01ba2d
Bastian Eicher <bastian@eicher.net>
2024-03-09 13:42:06 +0100

    Updated Visual Studio dependency in readme and build script

--- src/build.ps1
+++ src/build.ps1
@@ -4,14 +4,14 @@ pushd $PSScriptRoot
 
 function Find-MSBuild {
     if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
-        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 17.4
+        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 17.8
         if ($vsDir) { return "$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe" }
     }
 }
 
 function Run-MSBuild {
     $msbuild = Find-Msbuild
-    if (!$msbuild) { throw "You need Visual Studio 2022 v17.4 or newer to build this project" }
+    if (!$msbuild) { throw "You need Visual Studio 2022 v17.8 or newer to build this project" }
     . $msbuild @args
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
