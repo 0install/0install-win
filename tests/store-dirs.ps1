11fcf5d1f8c99ceb3663a355b972993909f23ab7
Bastian Eicher <bastian@eicher.net>
2026-08-03 00:26:45 +0200

    Made tests/store-dirs.ps1 more resilient

--- tests/store-dirs.ps1
+++ tests/store-dirs.ps1
@@ -1,9 +1,13 @@
-0install store add-dir $env:TEMP\implementations
+$dir = Join-Path ([Environment]::GetFolderPath("LocalApplicationData")) "0install-test-store"
+
+0install store add-dir $dir
 if ($LASTEXITCODE -ne 0) { throw "Exit Code: $LASTEXITCODE" }
 
 $output = 0install store list --batch
 if ($LASTEXITCODE -ne 0) { throw "Exit Code: $LASTEXITCODE" }
-if ($output -NotContains "ReadWrite: $env:TEMP\implementations") { throw "Wrong output" }
+if ($output -NotContains "ReadWrite: $dir") { throw "Wrong output: $output" }
 
-0install store remove-dir $env:TEMP\implementations
+0install store remove-dir $dir
 if ($LASTEXITCODE -ne 0) { throw "Exit Code: $LASTEXITCODE" }
+
+Remove-Item $dir
